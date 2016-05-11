3c94de7d63a456c11c59e8feabf298231d6d7094
Lance Olav Eastgate <lanceolav@lanceolav.com>
2016-05-11 21:06:10 +0200

    Added a front page with Hello World

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('I am alive!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import FrontPage from './pages/FrontPage';
+
+ReactDOM.render(
+  <FrontPage />,
+  document.getElementById('app')
+);
---
+++ src/pages/FrontPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FrontPage = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+});
+
+export default FrontPage;
