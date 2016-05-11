b8823d009bb05275d95dabebd4a754f1cd90f4cb
Lance Olav Eastgate <lanceolav@lanceolav.com>
2016-05-11 20:57:34 +0200

    Included mocha, chai, test folder, babel, react.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,33 @@
-module.exports = {
+import webpack from 'webpack';
+
+const config = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
+
+export default config;
