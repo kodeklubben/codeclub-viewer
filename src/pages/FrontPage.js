2def7383b50741ea5f4130d981859332bcb46243
Lance Olav Eastgate <lanceolav@lanceolav.com>
2016-05-12 22:09:59 +0200

    Adds a proof-of-concept loading of markdown with yaml front-matter.

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+const scratchPage = require('../../../oppgaver/src/scratch/forskyvning/forskyvning.md');
+
+console.log(scratchPage.frontmatter);
+
 const FrontPage = React.createClass({
+  createMarkup(){
+    return {
+      __html: scratchPage.content
+    };
+  },
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        <div dangerouslySetInnerHTML={this.createMarkup()} />
       </div>
     );
   }
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,10 @@
 import webpack from 'webpack';
 
+import MarkdownItAnchor from 'markdown-it-anchor';
+import MarkdownItAttrs from 'markdown-it-attrs';
+import MarkdownItHeaderSections from 'markdown-it-header-sections';
+import MarkdownItImplicitFigures from 'markdown-it-implicit-figures';
+
 const config = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
@@ -7,14 +12,31 @@ const config = {
     './src/index.js'
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'react-hot!babel'
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot!babel'
+      },
+      {
+        test: /\.md$/,
+        loader: 'combine-loader?' + JSON.stringify({
+          frontmatter: ['json-loader', 'front-matter-loader?onlyAttributes'],
+          content: ['html-loader', 'markdown-it-loader', 'front-matter?onlyBody']
+        })
+      },
+      {
+        // This loader is needed for some packages, e.g. sanitize-html (and markdown-it?)
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
+  },
+  resolveLoader: {
+    root: [__dirname+'/node_modules']
   },
   output: {
     path: __dirname + '/dist',
@@ -27,7 +49,17 @@ const config = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
-  ]
+  ],
+  'markdown-it': {
+    preset: 'commonmark',
+    //typographer: true,
+    use: [
+      MarkdownItAnchor,
+      MarkdownItAttrs,
+      MarkdownItHeaderSections,
+      MarkdownItImplicitFigures
+    ]
+  }
 };
 
 export default config;
