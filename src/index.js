a713451d6ece437d5341fcb2b07cd5f1a3a40a11
Lance Olav Eastgate <lanceolav@lanceolav.com>
2016-05-21 00:04:08 +0200

    Removed some old commented-out code

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import {render} from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
-
-// ReactDOM.render(
-//   <FrontPage />,
-//   document.getElementById('app')
-// );
-
-
-
 render(
   <Router routes={routes} history={browserHistory}/>,
   document.getElementById('app')
