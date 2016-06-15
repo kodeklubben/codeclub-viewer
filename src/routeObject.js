519329e95329a413077f65c9e92203ea3162d8af
Lance Olav Eastgate <lanceolav@lanceolav.com>
2016-06-16 00:38:50 +0200

    Move route object to separate file. Delete unused LessonPage. Fix bug in NavBar.

---
+++ src/routeObject.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+import App from './pages/App';
+
+export default function getRouteObject(
+  getComponentFrontPage,
+  getComponentPlaylist,
+  getComponentLessonPage
+) {
+  return (
+    <Route path="/" component={App}>
+      <IndexRoute getComponent={getComponentFrontPage}/>
+      <Route path="/:course" getComponent={getComponentPlaylist}/>
+      <Route path="/:course/:lesson/:file" getComponent={getComponentLessonPage}/>
+    </Route>
+  );
+}
