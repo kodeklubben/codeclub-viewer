b85a573abef57033139b1fb5de2c994d015a37f5
fredaas <fredaas86@gmail.com>
2016-06-24 16:31:51 +0200

    Outsource NavBar css.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,28 @@
 import React, {PropTypes} from 'react';
-import NavLink from './NavLink';
 
-const NavBar = React.createClass({
+import NavLink from './NavLink';
+import style from './nav.scss';
+
+export default class NavBar extends React.Component {
+
   render() {
     const params = this.props.params;
-    //console.log(this.props.params);
     const courseLink = params.course ? <NavLink to={`/${params.course}`}>{params.course}</NavLink> : null;
     const lessonLink = params.course && params.lesson && params.file ?
       <NavLink to={`/${params.course}/${params.lesson}/${params.file}`}>{params.file}</NavLink> : null;
+
     return (
-      <div style={{backgroundColor: '#B1DAAE', borderRadius: '5px', height:'40px', padding:'5px'}}>
+      <div className={style.container}>
         <p>
-          <NavLink to="/" onlyActiveOnIndex>Front Page</NavLink>
+          <NavLink to="/" onlyActiveOnIndex>Home</NavLink>
           {courseLink ? <span> / {courseLink}</span> : null}
           {lessonLink ? <span> / {lessonLink}</span> : null}
         </p>
       </div>
     );
   }
-});
+
+}
 
 NavBar.PropTypes = {
   params: PropTypes.shape({
@@ -27,5 +31,3 @@ NavBar.PropTypes = {
     file: PropTypes.string
   })
 };
-
-export default NavBar;
