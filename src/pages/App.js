7a01cc9192df7ef60beb632aaf16dfde9e2b100d
kristandre <kra1992@hotmail.com>
2016-06-26 22:43:27 +0200

    Remove Material-UI

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 
 const App = React.createClass({
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(null, {userAgent: 'all'})}>
-        <div>
-          <NavBar params={this.props.params}/>
-          {this.props.children}
-        </div>
-      </MuiThemeProvider>
+      <div>
+        <NavBar params={this.props.params}/>
+        {this.props.children}
+      </div>
     );
   }
 });
