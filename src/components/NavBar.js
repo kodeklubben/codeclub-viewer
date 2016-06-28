68ab431d317e968a620baa45d0d0602d44b6548a
fredaas <fredaas86@gmail.com>
2016-06-28 18:03:35 +0200

    Implement path and language structure.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,40 @@
 import React, {PropTypes} from 'react';
+
 import NavLink from './NavLink';
+import style from './navbar.scss';
 
 const NavBar = React.createClass({
+
+  getInitialState() {
+    return {
+      teacher: false
+    }
+  },
+
   render() {
     const params = this.props.params;
-    //console.log(this.props.params);
     const courseLink = params.course ? <NavLink to={`/${params.course}`}>{params.course}</NavLink> : null;
     const lessonLink = params.course && params.lesson && params.file ?
       <NavLink to={`/${params.course}/${params.lesson}/${params.file}`}>{params.file}</NavLink> : null;
+
     return (
-      <div style={{backgroundColor: '#B1DAAE', borderRadius: '5px', height:'40px', padding:'5px'}}>
-        <p>
-          <NavLink to="/" onlyActiveOnIndex>Front Page</NavLink>
-          {courseLink ? <span> / {courseLink}</span> : null}
-          {lessonLink ? <span> / {lessonLink}</span> : null}
-        </p>
+      <div className={style.container}>
+        <div className={style.languageGroup}>
+          <a className={style.language} href="#clicked">link</a>
+          <a className={style.language} href="#clicked">link</a>
+          <a className={style.language} href="#clicked">link</a>
+        </div>
+        <div className={style.header}>
+          <div className={style.path}>
+            <NavLink to="/" onlyActiveOnIndex>Front Page</NavLink>
+            {courseLink ? <span> / {courseLink}</span> : null}
+            {lessonLink ? <span> / {lessonLink}</span> : null}
+          </div>
+        </div>
       </div>
     );
   }
+
 });
 
 NavBar.propTypes = {
