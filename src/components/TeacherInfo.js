1fe068110c8e22829a3900f5679e4e7f84a647ec
fredaas <fredaas86@gmail.com>
2016-07-01 18:08:18 +0200

    Put TeacherInfo css in its own file.

--- src/components/TeacherInfo.js
+++ src/components/TeacherInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
-import styles from './ButtonGroup.scss';
+import styles from './TeacherInfo.scss';
 
 const TeacherInfo = React.createClass({
 
@@ -10,44 +10,42 @@ const TeacherInfo = React.createClass({
     const hrefTeacher = 'http://kidsakoder.no/skole/';
 
     return (
-      <div>
-        <div className={styles.teacherContainer}>
-          <div className={styles.sectionTeacher}>
-            <h3>Modus</h3>
-            <p>
-              Du er nå i lærermodus! <br /><br />
-              Klikk på elev/lærer knappen i navigasjonsmenyen for å skifte modus.
-              Når du er i lærer-modus vil skoleemner ligge øverst i oppgavefilteret.
-            </p>
-          </div>
+      <div className={styles.teacherContainer}>
+        <div className={styles.sectionTeacher}>
+          <h3>Modus</h3>
+          <p>
+            Du er nå i lærermodus! <br /><br />
+            Klikk på elev/lærer knappen i navigasjonsmenyen for å skifte modus.
+            Når du er i lærer-modus vil skoleemner ligge øverst i oppgavefilteret.
+          </p>
+        </div>
 
-          <div className={styles.sectionTeacherInline}>
-            <h3>Lærere</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a className={styles.link} href={hrefTeacher}>Lær mer</a>
-          </div>
+        <div className={styles.sectionTeacherInline}>
+          <h3>Lærere</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a className={styles.link} href={hrefTeacher}>Lær mer</a>
+        </div>
 
-          <div className={styles.sectionTeacherInline}>
-            <h3>Veiledere</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a className={styles.link} href={hrefAssistant}>Lær mer</a>
-          </div>
+        <div className={styles.sectionTeacherInline}>
+          <h3>Veiledere</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a className={styles.link} href={hrefAssistant}>Lær mer</a>
         </div>
       </div>
     );
