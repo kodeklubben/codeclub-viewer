395bbdf86b54274643b261a5c28019ea0fcd0709
kristandre <kra1992@hotmail.com>
2016-07-02 00:58:13 +0200

    Use bootstrap grid to create layout

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -2,20 +2,20 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import CourseList from '../components/CourseList/CourseList';
 import LessonFilter from '../components/Filter/LessonFilter';
-import styles from './FrontPage.scss';
 import * as actionCreators from '../action_creators';
 import {getFilteredCourses} from '../selectors/course';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 export const FrontPage = React.createClass({
   render() {
     return (
-      <div className={styles.content}>
-        <div className={styles.leftColumn}>
-          <LessonFilter {...this.props}/>
-        </div>
-        <div className={styles.rightColumn}>
-          <CourseList courses={this.props.courses}/>
+      <div className='container-fluid'>
+        <div className="row">
+          <div className='col-xs-12 col-sm-4 col-md-3 col-lg-2'>
+            <LessonFilter {...this.props}/>
+          </div>
+          <div className='col-xs-12 col-sm-8 col-md-9 col-lg-10'>
+            <CourseList courses={this.props.courses}/>
+          </div>
         </div>
       </div>
     );
@@ -38,4 +38,4 @@ function mapStateToProps(state) {
 export const FrontPageContainer = connect(
   mapStateToProps,
   actionCreators
-)(withStyles(styles)(FrontPage));
+)(FrontPage);
