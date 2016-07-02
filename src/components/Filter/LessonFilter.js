53fb60c69eceaf8ac5cc57bbf3e5597b5975d157
kristandre <kra1992@hotmail.com>
2016-07-02 18:36:36 +0200

    Remove screen width code

--- src/components/Filter/LessonFilter.js
+++ src/components/Filter/LessonFilter.js
@@ -2,34 +2,7 @@ import React, {PropTypes} from 'react';
 import FilterGroup from './FilterGroup';
 import styles from './Filter.scss';
 
-const BREAK_POINT = 768;
-
 const LessonFilter = React.createClass({
-  getInitialState: function() {
-    const screenWidth = window.innerWidth;
-    return {
-      showFilter: screenWidth > BREAK_POINT,
-      screenWidth
-    };
-  },
-  handleResize: function(e) {
-    const screenWidth = window.innerWidth;
-    this.setState({
-      showFilter: screenWidth > BREAK_POINT,
-      screenWidth
-    });
-  },
-
-  componentDidMount: function() {
-    window.addEventListener('resize', this.handleResize);
-  },
-
-  componentWillUnmount: function() {
-    window.removeEventListener('resize', this.handleResize);
-  },
-  handleHeadingClicked() {
-    if(this.state.screenWidth <= BREAK_POINT) this.setState({showFilter: !this.state.showFilter});
-  },
   render(){
     const filter = this.props.filter;
     const filterGroups = Object.keys(filter).map((groupName, idx) => {
@@ -43,10 +16,10 @@ const LessonFilter = React.createClass({
     return (
       <div>
         <div className='panel panel-success'>
-          <div className={'panel-heading ' + styles.filterHeading} onClick={this.handleHeadingClicked}>
-            <h3 className='panel-title'>{this.state.screenWidth > BREAK_POINT ? 'Filter' : 'Vis filter'}</h3>
+          <div className={'panel-heading ' + styles.filterHeading}>
+            <h3 className='panel-title'>Filter</h3>
           </div>
-          <div className={'panel-body ' + styles.noPaddingTop} style={this.state.showFilter ? null : {display: 'none'}}>
+          <div className={'panel-body ' + styles.noPaddingTop}>
             {filterGroups}
           </div>
         </div>
