3d9d40ce7ea3bb95d134e82fa315752af61de26f
kristandre <kra1992@hotmail.com>
2016-07-03 14:54:52 +0200

    Use react-bootstrap

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -4,6 +4,14 @@ import CourseList from '../components/CourseList/CourseList';
 import LessonFilter from '../components/Filter/LessonFilter';
 import * as actionCreators from '../action_creators';
 import {getFilteredCourses} from '../selectors/course';
+import {
+  Button,
+  Col,
+  Collapse,
+  Glyphicon,
+  Grid,
+  Row
+} from 'react-bootstrap';
 
 export const FrontPage = React.createClass({
   getInitialState() {
@@ -11,27 +19,37 @@ export const FrontPage = React.createClass({
   },
   render() {
     return (
-      <div className='container-fluid'>
-        <div className="row">
-          
+      <Grid fluid={true}>
+        <Row>
+
           {/*Filter desktop*/}
-          <div className='hidden-xs col-sm-4 col-md-3 col-lg-2'>
+          <Col xsHidden sm={4} md={3} lg={2}>
+            <br/>
             <LessonFilter {...this.props}/>
-          </div>
-          
+          </Col>
+
           {/*Filter mobile*/}
-          <div className='visible-xs col-xs-12'>
-            <button onClick={() => this.setState({showMobileFilter: !this.state.showMobileFilter})}>Vis/skjul filter</button>
-            <div style={this.state.showMobileFilter ? null : {display: 'none'}}>
-              <LessonFilter {...this.props}/>
-            </div>
-          </div>
-          
-          <div className='col-xs-12 col-sm-8 col-md-9 col-lg-10'>
+          <Col smHidden mdHidden lgHidden xs={12}>
+            <br/>
+            <Button bsStyle='success' onClick={() => this.setState({showMobileFilter: !this.state.showMobileFilter})}>
+              <Glyphicon glyph={this.state.showMobileFilter ? 'chevron-down' : 'chevron-right'}/>
+              Vis/skjul filter
+            </Button>
+            <br/>
+            <br/>
+            <Collapse in={this.state.showMobileFilter}>
+              <div>
+                <LessonFilter {...this.props}/>
+              </div>
+            </Collapse>
+          </Col>
+
+          <Col xs={12} sm={8} md={9} lg={10}>
+            <h2>Kurs</h2>
             <CourseList courses={this.props.courses}/>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 });
