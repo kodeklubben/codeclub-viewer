b11281f7ba7741af0e161ab3c448050d00facb1f
kristandre <kra1992@hotmail.com>
2016-07-04 12:50:54 +0200

    Extend bootstrap with panel-student

--- src/components/Filter/LessonFilter.js
+++ src/components/Filter/LessonFilter.js
@@ -1,7 +1,5 @@
 import React, {PropTypes} from 'react';
 import FilterGroup from './FilterGroup';
-import styles from './LessonFilter.scss';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 const LessonFilter = React.createClass({
   render(){
@@ -9,20 +7,16 @@ const LessonFilter = React.createClass({
     const filterGroups = Object.keys(filter).map((groupName, idx) => {
       const tagItems = filter[groupName];
       return (
-        <div key={idx} className={styles.bottomBorder}>
-          <FilterGroup groupName={groupName} tagItems={tagItems} onFilterCheck={this.props.onFilterCheck}/>
-        </div>
+        <FilterGroup key={idx} groupName={groupName} tagItems={tagItems} onFilterCheck={this.props.onFilterCheck}/>
       );
     });
     return (
-      <div>
-        <div className='panel panel-success'>
-          <div className={'panel-heading ' + styles.filterHeading}>
-            <h3 className='panel-title'>Filter</h3>
-          </div>
-          <div className={'panel-body ' + styles.noPaddingTop}>
-            {filterGroups}
-          </div>
+      <div className='panel panel-student'>
+        <div className='panel-heading'>
+          <h3 className='panel-title'>Filter</h3>
+        </div>
+        <div className='panel-body'>
+          {filterGroups}
         </div>
       </div>
     );
@@ -34,4 +28,4 @@ LessonFilter.propTypes = {
   onFilterCheck: PropTypes.func
 };
 
-export default withStyles(styles)(LessonFilter);
+export default LessonFilter;
