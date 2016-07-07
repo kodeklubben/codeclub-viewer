23bffee3f56e351cc7c783319dd012952903d895
fredaas <fredaas86@gmail.com>
2016-07-07 11:37:49 +0200

    Remove homemade nav. Add react bootstrap navigation components.

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -4,8 +4,11 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import NavLink from './NavLink';
 import styles from './NavBar.scss';
 import ToggleButton from './ToggleButton';
-import {Grid, Row} from 'react-bootstrap';
 import FlagGroup from './FlagGroup';
+
+import Navbar from 'react-bootstrap/lib/Navbar';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
 
 const NavBar = React.createClass({
 
@@ -22,24 +25,25 @@ const NavBar = React.createClass({
       <NavLink to={`/${params.course}/${params.lesson}/${params.file}`}>{params.file}</NavLink> : null;
 
     return (
-      <Grid fluid={true}>
-        <Row>
-          <FlagGroup />
-        </Row>
-        <Row>
-          <div className={styles.header}>
-            <div className={styles.left}>
-              <NavLink to="/" onlyActiveOnIndex>Front Page</NavLink>
-              {courseLink ? <span> / {courseLink}</span> : null}
-              {lessonLink ? <span> / {lessonLink}</span> : null}
-            </div>
-            <div className={styles.right}>
-              <input type='text' placeholder='Søk' />
-              {<ToggleButton from='ELEV' to='LÆRER' onClick={() => this.setState({ student: !this.state.student })} />}
-            </div>
-          </div>
-        </Row>
-      </Grid>
+      <Navbar className={styles.navbar} fluid={true}>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <NavLink to="/" onlyActiveOnIndex>Front Page</NavLink>
+            {courseLink ? <span> / {courseLink}</span> : null}
+            {lessonLink ? <span> / {lessonLink}</span> : null}
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Form pullRight>
+            <FormGroup>
+              <FormControl type="text" placeholder="Search" />
+            </FormGroup>
+            {' '}
+            <ToggleButton from='ELEV' to='LÆRER' onClick={() => this.setState({ student: !this.state.student })} />
+          </Navbar.Form>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 
