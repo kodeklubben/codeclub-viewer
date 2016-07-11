db059fb462ca8f3c903e72dbd2af08b86447678a
fredaas <fredaas86@gmail.com>
2016-07-11 12:36:23 +0200

    Fix wrong filenames.

--- src/assets/graphics/FlagDenmark.js
+++ src/assets/graphics/FlagDenmark.js
@@ -4,15 +4,15 @@ const FlagDenmark = React.createClass({
 
   render() {
     const color = {
-      s0: {fill: '#c60c30'},
-      s1: {fill: '#ffffff'}
+      s0: {fill: '#006aa7'},
+      s1: {fill: '#fecc00'}
     };
 
     return (
       <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 500 350">
         <rect style={color.s0} width="500" height="350"/>
-        <rect x="156.3" style={color.s1} width="62.5" height="350"/>
-        <rect y="140" style={color.s1} width="500" height="70"/>
+        <rect x="162.2" style={color.s1} width="54.1" height="350"/>
+        <rect y="150" style={color.s1} width="500" height="50"/>
       </svg>
     );
   }
