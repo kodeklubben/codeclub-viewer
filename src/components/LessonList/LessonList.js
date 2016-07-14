bb47bb547c8643a6559bf6bd0c879bdd8ad32797
Kristoffer Ravik Andresen <kra1992@hotmail.com>
2016-07-14 14:18:25 +0200

    Show links to teacher notes in PlaylistPage (#99)
    * Show links to teacher notes in PlaylistPage
    
    Add readme path to lessons.
    Show link to readme in teacher mode only.
    
    * Add isStudentMode to LessonItem propTypes
    
    * Move button style to LessonItem.scss
    
    * Route README paths to LessonPage
    
    Use absolute paths instead of relative paths on links to lessons and
    readme pages.
    Show lesson name instead of file name in breadcrumbs in navbar
    
    * Add newline at end of LessonItem.scss

--- src/components/LessonList/LessonList.js
+++ src/components/LessonList/LessonList.js
@@ -1,5 +1,5 @@
 import React, {PropTypes} from 'react';
-import LessonItem from './LessonItem';
+import {LessonItemContainer} from './LessonItem';
 import {getLevelName} from '../../util';
 
 const LessonList = React.createClass({
@@ -18,7 +18,9 @@ const LessonList = React.createClass({
         <h3>{level + '. ' + getLevelName(level)}</h3>
         <ul className='list-group'>
           {lessons.map((lesson, idx) =>
-            <LessonItem key={idx} lesson={lesson} constraints={this.generateMockConstraints(idx)}/>
+            lesson.indexed ?
+              <LessonItemContainer key={idx} lesson={lesson} constraints={this.generateMockConstraints(idx)}/>
+              : null
           )}
         </ul>
       </div>
