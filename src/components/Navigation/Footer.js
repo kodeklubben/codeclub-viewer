a71070cda8e730a8ff10c80d52e8963b5ff93375
fredaas <fredaas86@gmail.com>
2016-07-20 13:20:58 +0200

    Add URLs.

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -8,13 +8,20 @@ import styles from './Footer.scss';
 const Footer = React.createClass({
 
   render() {
+    const url = [
+      'https://github.com/kodeklubben/oppgaver/',
+      'http://kidsakoder.no/',
+      'https://github.com/kodeklubben/kodeklubben.github.io/archive/master.zip',
+      'http://kidsakoder.no/2015/07/03/kodeklubben-trondheim-utvikler-materiell-i-sommer/'
+    ];
+
     return (
       <Grid fluid={true} className={styles.container}>
         <Row className={styles.center}>
           <p>Bidra?</p>
         </Row>
         <Row className={styles.center}>
-          <a href='#clicked'>
+          <a href={url[0]}>
             <img className={styles.svg} src={'src/assets/graphics/github.svg'}/>
           </a>
         </Row>
@@ -23,15 +30,19 @@ const Footer = React.createClass({
         </Row>
         <Row className={styles.center}>
           <div className={styles.linkGroup}>
-            <a href='#clicked'>kidsakoder.no</a>
+            <a href={url[1]}>kidsakoder.no</a>
           </div>
           <div className={styles.linkGroup}>
-            <a href='#clicked'>Last ned alle kurs som zip-fil</a>
+            <a href={url[2]}>Last ned alle kurs som zip-fil</a>
           </div>
         </Row>
         <Row className={styles.center}>
-          <img className={styles.img} href='#clicked' src={'src/assets/graphics/smn.jpg'}/>
-          <img className={styles.img} href='#clicked' src={'src/assets/graphics/ibok.jpg'}/>
+          <a href={url[3]}>
+            <img className={styles.img} src={'src/assets/graphics/smn.jpg'}/>
+          </a>
+          <a href={url[3]}>
+            <img className={styles.img} src={'src/assets/graphics/ibok.jpg'}/>
+          </a>
         </Row>
       </Grid>
     );
