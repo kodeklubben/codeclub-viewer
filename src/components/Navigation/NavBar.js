de71a067c941b0112f6227d673a13c0eee631eaa
Fredrik Ås <fredaas@users.noreply.github.com>
2016-07-22 13:26:25 +0200

    Change color theme when in teacher/student mode. (#108)
    * Change background color on navbar when in teacher mode.
    
    * Use single quotes.
    
    * Only show teacher/student buttons when in student mode.
    
    * Change color on filter when in teacher mode.
    
    * Change color on home icon when in teacher mode.
    
    * Change color on hamburger button when in teacher mode.
    
    * Fix NavLink to actually change color when in teacher mode.
    
    * Fix too long lines.
    
    * Use global button classes. Change teacher color theme.
    
    * Access state directly in NavLink.
    
    * Use setModeTeacher instead of changeMode.
    
    * Fix filter collapse button theme.
    
    * Fix too long line.
    
    * Change toggle text.
    
    * Make toggle mode more intuitive.
    
    * Fix yet another eslint complaint.
    
    * Add button group.
    
    * Enable button groups.
    
    * Remove changeMode. Add setModeStudent and setModeTeacher.
    
    * Make it more visible that a button is active.
    
    * Add toggle text.
    
    * Fix eslint error.
    
    * Fix stylelint errors.

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
-import {changeMode} from '../../action_creators';
+import {setModeStudent, setModeTeacher} from '../../action_creators';
 
 import Button from 'react-bootstrap/lib/Button';
 import Grid from 'react-bootstrap/lib/Grid';
@@ -9,14 +9,19 @@ import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import FormControl from 'react-bootstrap/lib/FormControl';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 
 import NavLink from './NavLink';
 import FlagGroup from './FlagGroup';
 
 export const NavBar = React.createClass({
+
   render() {
     const params = this.props.params;
-    const courseLink = params.course ? <NavLink to={`/${params.course}`}>{params.course}</NavLink> : null;
+    const courseLink = params.course ?
+      <NavLink to={`/${params.course}`}>
+        {params.course}
+      </NavLink> : null;
     const lessonLink = params.course && params.lesson && params.file ?
       <NavLink to={`/${params.course}/${params.lesson}/${params.file}`}>
         {(params.lesson).replace(/_/g, ' ')}
@@ -25,10 +30,10 @@ export const NavBar = React.createClass({
     return (
       <Grid fluid={true}>
         <Row>
-          <FlagGroup />
+          <FlagGroup/>
         </Row>
         <Row>
-          <Navbar fluid={true}>
+          <Navbar className={this.props.isStudentMode ? null : 'navbar-teacher'} fluid={true}>
             <Navbar.Header>
               <Navbar.Brand>
                 <NavLink to="/" onlyActiveOnIndex>
@@ -37,7 +42,7 @@ export const NavBar = React.createClass({
                 {courseLink ? <span> / {courseLink}</span> : null}
                 {lessonLink ? <span className="hidden-xs"> / {lessonLink}</span> : null}
               </Navbar.Brand>
-              <Navbar.Toggle />
+              <Navbar.Toggle className={this.props.isStudentMode ? null : 'toggle-teacher'}/>
             </Navbar.Header>
             <Navbar.Collapse>
               <Navbar.Form pullRight>
@@ -45,11 +50,19 @@ export const NavBar = React.createClass({
                   <FormControl type="text" placeholder="Søk"/>
                 </FormGroup>
                 {' '}
-                {this.props.isStudentMode ?
-                  <Button bsStyle="primary" onClick={() => this.props.changeMode()}>LÆRER</Button>
-                  :
-                  <Button bsStyle="success" onClick={() => this.props.changeMode()}>ELEV</Button>
-                }
+                <div className='btn-toggle'>
+                  <div>Velg modus:</div>
+                  <ButtonGroup className='btn-group'>
+                    <Button className='btn-student-nav' active={this.props.isStudentMode}
+                      onClick={() => this.props.setModeStudent()}>
+                      ELEV
+                    </Button>
+                    <Button className="btn-teacher-nav" active={!this.props.isStudentMode}
+                      onClick={() => this.props.setModeTeacher()}>
+                      LÆRER
+                    </Button>
+                  </ButtonGroup>
+                </div>
               </Navbar.Form>
             </Navbar.Collapse>
           </Navbar>
@@ -66,7 +79,8 @@ NavBar.propTypes = {
     lesson: PropTypes.string,
     file: PropTypes.string
   }),
-  changeMode: PropTypes.func,
+  setModeStudent: PropTypes.func,
+  setModeTeacher: PropTypes.func,
   isStudentMode: PropTypes.bool
 };
 
@@ -79,6 +93,7 @@ function mapStateToProps(state) {
 export const NavBarContainer = connect(
   mapStateToProps,
   {
-    changeMode
+    setModeStudent,
+    setModeTeacher
   }
 )(NavBar);
