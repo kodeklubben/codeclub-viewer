bd5564cb54798175b36c44cb507cdd275c529f99
arve0 <arve.seljebu@gmail.com>
2016-07-22 16:47:42 +0200

    highlight: render scratchblocks serverside

--- src/highlighting.js
+++ src/highlighting.js
@@ -1,9 +1,10 @@
 import hljs from 'highlight.js';
+import scratchblocks from 'scratchblocks';
 
 export default function (code, lang) {
   if (lang === 'blocks') {
     // set class on pre, to avoid background coloring of scratch code
-    return `<pre class="blocks">${code}</pre>`;
+    return `<pre class="blocks">${scratchblocks(code)}</pre>`;
   }
 
   if (lang && hljs.getLanguage(lang)) {
