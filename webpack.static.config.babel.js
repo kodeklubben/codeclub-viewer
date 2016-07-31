c238b34f4ba3024eff4c4438c3d33e33a185e317
arve0 <arve.seljebu@gmail.com>
2016-07-31 14:38:01 +0200

    Revert "add process global to static rendering"
    This reverts commit faab0704ab7cefb7abcc18af3d23486d48c396d1.

--- webpack.static.config.babel.js
+++ webpack.static.config.babel.js
@@ -35,18 +35,7 @@ import StaticSiteGeneratorPlugin from 'static-site-generator-webpack-plugin';
 // CONSTANTS //
 ///////////////
 
-const scope = {
-  window: {},
-  /**
-   * We use the `process` global to check if we are on client or not,
-   * in example when rendering scratchblocks.
-   *
-   * if (!process) {
-   *   // do something that requires browser DOM, etc
-   * }
-   */
-  process: {}
-};
+const scope = {window: {}};
 const locals = {};
 
 
