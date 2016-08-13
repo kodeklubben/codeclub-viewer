4a988208e5e8550b36b603447f0ecada711ef747
Lance Olav Eastgate <lanceolav@lanceolav.com>
2016-08-13 16:40:10 +0200

    Get value from file url-path-prefix.config if it exists, instead of hardcoded into webpack.base.config.babel.js.

--- webpack.base.config.babel.js
+++ webpack.base.config.babel.js
@@ -25,14 +25,18 @@ import MarkdownItAttrs from 'markdown-it-attrs';
 import MarkdownItHeaderSections from 'markdown-it-header-sections';
 import MarkdownItImplicitFigures from 'markdown-it-implicit-figures';
 import highlight from './src/highlighting.js';
+const fs = require('fs');
 
 ///////////////
 // CONSTANTS //
 ///////////////
 
-// Use 'subDir' to serve the site from a subdir,
-// e.g. subDir='beta' for http://kodeklubben.github.io/beta
-const subDir = ''; // No slashes at start or end
+// Use 'subDir' to serve the site from a subdir, e.g. subDir='beta' for http://kodeklubben.github.io/beta
+// The value of subDir is read from the file 'url-path-prefix.config' in the root folder, if it exists.
+const subDirFile = './url-path-prefix.config';
+var subDir = fs.existsSync(subDirFile) ? fs.readFileSync(subDirFile, 'utf8').trim() : '';
+if (subDir.startsWith('/')) { subDir = subDir.slice(1); }
+if (subDir.endsWith('/')) { subDir = subDir.slice(0, -1); }
 
 export const buildDir = path.join('dist', subDir);
 // Webpack needs final slash in publicPath to rewrite relative paths correctly
