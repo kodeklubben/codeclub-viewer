4d3d30d6ed0f838fb948ece63b1120775fdd1875
Lance Olav Eastgate <lanceolav@lanceolav.com>
2016-11-04 07:02:36 +0100

    Changed travis from npm to yarn

--- travis_build_prod.sh
+++ travis_build_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Building production files while checking for errors..."
 
-if npm run build:prod:travis | grep --after-context=5 ERROR
+if yarn run build:prod:travis | grep --after-context=5 ERROR
 then
    echo "Build FAILED!"
    exit 1
