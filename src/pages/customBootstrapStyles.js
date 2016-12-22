dfb1c2f7db0a8581560bfa2859d9e81b83df718c
Lance Olav Eastgate <lanceolav@lanceolav.com>
2016-12-22 21:54:51 +0100

    Simplify navbar, and related css, as well as writing lessonfilter in react-bootstrap.

---
+++ src/pages/customBootstrapStyles.js
@@ -0,0 +1,11 @@
+// Add custom styles to react-bootstrap, so that they can be used via bsStyle without giving a warning,
+// e.g. for Panel, with the custom css-style .panel-student (defined globally somewhere)
+// it could be used as <Panel bsStyle={student}>...</Panel>
+
+const bootstrapUtils = require('react-bootstrap/lib/utils/bootstrapUtils');
+
+import Panel from 'react-bootstrap/lib/Panel';
+import Button from 'react-bootstrap/lib/Button';
+
+bootstrapUtils.addStyle(Panel, 'student', 'teacher');
+bootstrapUtils.addStyle(Button, 'student', 'teacher', 'language-student', 'language-teacher');
