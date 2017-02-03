91831390caf17e59d564491bc5d40a31865c82da
krskog <k.skog@online.no>
2017-02-03 10:25:24 +0100

    Added some text

--- src/components/ImprovePage.js
+++ src/components/ImprovePage.js
@@ -15,16 +15,17 @@ const ImprovePage = React.createClass({
       
       <Grid fluid={true} className={this.props.isStudentMode ? styles.infoBoxTeacher : styles.infoBoxStudent}>
           <div className={styles.infoBox}>
+            <div>
+              <h3>Forbedre denne siden</h3>
+              <p>Funnet en feil? Kunne noe vært bedre? Hvis ja, vennligst gi oss tilbakemelding ved å lage en sak på Github eller fiks feilen selv om du kan. Vi er takknemlige for enhver tilbakemelding!</p>
+            </div>
             <div className={styles.infoBoxRow}>
-              <div>
-                <h2 className={styles.fontInfoBox}>Problemer</h2>
-                
-                <a className={styles.link} href={url.problemer}>Lær mer</a>
-              </div>
-              <div>
-                <h2 className={styles.fontInfoBox}>Kode</h2>
-                <a className={styles.link} href={url.kode}>Lær mer</a>
-              </div>
+                <div>
+                  <a className={styles.link} href={url.problemer}>Rapporter et problem</a>
+                </div>
+                <div>
+                  <a className={styles.link} href={url.kode}>Vis koden og fiks selv</a>
+                </div>
             </div>
           </div>
       </Grid>
