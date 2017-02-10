f379581c02af48c5d65599942b7e7fa6c4425471
Skagevang <hero3@hotmail.com>
2017-02-10 13:16:22 +0100

    Styled button and added react-tooltip for the text. This shows when hovering the button. Final step is to change text inside the tooltip.

--- src/components/Filter/LessonFilter.js
+++ src/components/Filter/LessonFilter.js
@@ -4,6 +4,7 @@ import {onFilterCheck, resetFilter} from '../../action_creators';
 import Button from 'react-bootstrap/lib/Button';
 import Panel from 'react-bootstrap/lib/Panel';
 import FilterGroup from './FilterGroup';
+import ReactTooltip from 'react-tooltip';
 
 export const LessonFilter = React.createClass({
   render(){
@@ -14,12 +15,19 @@ export const LessonFilter = React.createClass({
         <FilterGroup key={idx} groupName={groupName} tagItems={tagItems} onFilterCheck={this.props.onFilterCheck}/>
       );
     });
+    
+        /*Include a help-button for filter, e.g. a question mark next to the title, explaining how the filter works, and what the tags mean. This includes explaining that groups intersect, and but tags within a group are a union, and that if nothing is checkmarked within a group, all are shown (rather than none).
+
+        It must also explain what that tags mean; e.g. does "android" mean that it shows lessons that requires an android device to run, or and android device to program on, and does it mean that it only works on android, or that it also works on android?
+
+        If possible, we should avoid explaining what each tag means, and just have a general rule of how tags behave.*/
 
     const title = <h3>Filter</h3>;
     const bsStyle = (this.props.isStudentMode ? 'student' : 'teacher');
     return (
-      <div>
-        <Button className="btn-sm-fixed-width pull-right" onClick={() => console.log('Her kan det stå sykt my shit om filter')}>?</Button>
+      <div>  
+        <Button data-place="right" style={{ marginTop: 2, marginRight: 2 }} className="pull-right" data-effect="solid" data-multiline="true" data-tip="I filteret ser man hvor mange oppgaver man kan velge i parentesene bak valgene i Operativsystem og Tema.<br> Velger man f.eks. Android, under Operativsystem, får man opp de kursene som kun fungerer på Anroid<br>Velger man f.eks. App, under Tema, får man opp de kursene som ">?</Button>
+        <ReactTooltip />
         <Panel header={title} bsStyle={bsStyle}>
           {filterGroups}
           <br/>
