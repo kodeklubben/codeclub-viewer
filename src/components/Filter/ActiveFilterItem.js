a63045d801eaf10b6c7472909e538080bc061d16
agnete <agnetedjupvik@gmail.com>
2017-02-28 10:42:43 +0100

    Convert ActiveFilterItem to pure function

--- src/components/Filter/ActiveFilterItem.js
+++ src/components/Filter/ActiveFilterItem.js
@@ -5,23 +5,21 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Button from 'react-bootstrap/lib/Button';
 import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
+const ActiveFilterItem = (props) => {
+  const tagItem = capitalize(props.tagItem);
 
-const ActiveFilterItem = React.createClass({
-  render(){
-    const tagItem = capitalize(this.props.tagItem);
-
-    return (
-      <span>
-        <Button className={styles.item} onClick={this.props.onClick}>
-          <Glyphicon glyph={'glyphicon glyphicon-remove'} className={styles.remove}/> {tagItem}
-        </Button>
-      </span>
-    );
-  }
-});
+  return (
+    <span>
+      <Button className={styles.item} onClick={this.props.onClick}>
+        <Glyphicon glyph={'glyphicon glyphicon-remove'} className={styles.remove}/> {tagItem}
+      </Button>
+    </span>
+  );
+};
 
 ActiveFilterItem.propTypes = {
   tagItem: PropTypes.string,
+  onClick: PropTypes.func.isRequired
 };
 
 export default withStyles(styles)(ActiveFilterItem);
