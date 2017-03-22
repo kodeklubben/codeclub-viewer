37687f650f35fa4ed14cd7f2e4c4ef75b28368bc
Skagevang <hero3@hotmail.com>
2017-03-22 15:27:13 +0100

    hide textbox if userprogress in state is true

--- src/components/FrontPage/TeacherInfobox.js
+++ src/components/FrontPage/TeacherInfobox.js
@@ -1,9 +1,9 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Collapse from 'react-bootstrap/lib/Collapse';
 import Button from 'react-bootstrap/lib/Button';
 import {doNotShowAgain} from '../../localStorage';
-
 import styles from './TeacherInfobox.scss';
 
 const TeacherInfobox = React.createClass({
@@ -13,50 +13,67 @@ const TeacherInfobox = React.createClass({
       assistant: 'http://kidsakoder.no/kodeklubben/',
       teacher: 'http://kidsakoder.no/skole/'
     };
-
-    return (
-      <div className={styles.infoBoxContainer}>
-        <Collapse in={!this.props.isStudentMode}>
-          <div className={styles.infoBox}>
-            <div className={styles.infoBoxRow}>
-              <div className={styles.infoBoxItem}>
-                Du er nå lærermodus!
-                <br /><br />
-                Klikk på elev/lærer knappen i navigasjonsmenyen for å skifte modus.
-                Når du er i lærer-modus vil skoleemner ligge øverst i oppgavefilteret.
+    
+    if(this.props.userProgress === "false") {
+      return (
+        <div className={styles.infoBoxContainer}>
+          <Collapse in={!this.props.isStudentMode}>
+            <div className={styles.infoBox}>
+              <div className={styles.infoBoxRow}>
+                <div className={styles.infoBoxItem}>
+                  Du er nå lærermodus!
+                  <br /><br />
+                  Klikk på elev/lærer knappen i navigasjonsmenyen for å skifte modus.
+                  Når du er i lærer-modus vil skoleemner ligge øverst i oppgavefilteret.
+                </div>
+              </div>
+              <div className={styles.infoBoxRow}>
+                <div>
+                  <h3>Lærer</h3>
+                  text text text text text text text text text text text
+                  text text text text text text text text text text text
+                  text text text text text text text text text text text
+                  text text text text text text text text text text text
+                  <br /><br />
+                  <a className={styles.link} href={url.teacher}>Lær mer</a>
+                </div>
+                <div>
+                  <h3>Veileder</h3>
+                  text text text text text text text text text text text
+                  text text text text text text text text text text text
+                  text text text text text text text text text text text
+                  text text text text text text text text text text text
+                  <br /><br />
+                  <a className={styles.link} href={url.assistant}>Lær mer</a>
+                </div>
+                <Button onClick={doNotShowAgain}>Ikke vis igjen</Button>
               </div>
             </div>
-            <div className={styles.infoBoxRow}>
-              <div>
-                <h3>Lærer</h3>
-                text text text text text text text text text text text
-                text text text text text text text text text text text
-                text text text text text text text text text text text
-                text text text text text text text text text text text
-                <br /><br />
-                <a className={styles.link} href={url.teacher}>Lær mer</a>
-              </div>
-              <div>
-                <h3>Veileder</h3>
-                text text text text text text text text text text text
-                text text text text text text text text text text text
-                text text text text text text text text text text text
-                text text text text text text text text text text text
-                <br /><br />
-                <a className={styles.link} href={url.assistant}>Lær mer</a>
-              </div>
-              <Button onClick={doNotShowAgain}>Ikke vis igjen</Button>
-            </div>
-          </div>
-        </Collapse>
-      </div>
-    );
+          </Collapse>
+        </div>
+      );
+    }
+    else {
+      return (
+      <div></div>);
+    }
   }
 
 });
 
 TeacherInfobox.propTypes = {
-  isStudentMode: PropTypes.bool
+  isStudentMode: PropTypes.bool,
+  userProgress: PropTypes.string
 };
 
-export default withStyles(styles)(TeacherInfobox);
+function mapStateToProps(state) {
+  return {
+    userProgress: state.userProgress
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  {doNotShowAgain}
+)(withStyles(styles)(TeacherInfobox));
+      
