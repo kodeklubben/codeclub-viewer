55801247c09ecfc09c768c12d2603aadaa074a91
Lance Olav Eastgate <lanceolav@lanceolav.com>
2017-03-27 20:27:39 +0200

    Issue 229: Remove contextTypes, and make CourseItem a pure component.

--- src/components/CourseList/CourseItem.js
+++ src/components/CourseList/CourseItem.js
@@ -5,31 +5,25 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import {Link} from 'react-router';
 import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
-const CourseItem = React.createClass({
-  contextTypes: {
-    router: React.PropTypes.object
-  },
-  render() {
-    const course = this.props.course;
-    const isExternal = course.hasOwnProperty('externalLink');
-    return (
-      <div>
-        {isExternal ?
-          <a className={styles.courseItem} href={course.externalLink} target='_blank'>
-            <img className={styles.courseLogo} src={course.iconPath}/>
-            <span className={styles.courseName}>{course.name} <Glyphicon glyph='new-window'/></span>
-          </a>
-          :
-          <Link className={styles.courseItem} to={course.path}>
-            <img className={styles.courseLogo} src={course.iconPath}/>
-            <span className={styles.courseName}>{course.name}</span>
-            <span className={styles.lessonCount}>Oppgaver: {course.lessonCount}</span>
-          </Link>
-        }
-      </div>
-    );
-  }
-});
+const CourseItem = ({course}) => {
+  const isExternal = course.hasOwnProperty('externalLink');
+  return (
+    <div>
+      {isExternal ?
+        <a className={styles.courseItem} href={course.externalLink} target='_blank'>
+          <img className={styles.courseLogo} src={course.iconPath}/>
+          <span className={styles.courseName}>{course.name} <Glyphicon glyph='new-window'/></span>
+        </a>
+        :
+        <Link className={styles.courseItem} to={course.path}>
+          <img className={styles.courseLogo} src={course.iconPath}/>
+          <span className={styles.courseName}>{course.name}</span>
+          <span className={styles.lessonCount}>Oppgaver: {course.lessonCount}</span>
+        </Link>
+      }
+    </div>
+  );
+};
 
 CourseItem.propTypes = {
   course: PropTypes.shape({
