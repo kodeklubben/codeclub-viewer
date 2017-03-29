8ca19cf48d08636740b979b1078fe20f36ce04e6
Skagevang <hero3@hotmail.com>
2017-03-29 11:05:05 +0200

    merge teacherinfobox

--- src/components/FrontPage/TeacherInfobox.js
+++ src/components/FrontPage/TeacherInfobox.js
@@ -1,11 +1,9 @@
 import React, {PropTypes} from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import Collapse from 'react-bootstrap/lib/Collapse';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import {getTeacherInfo} from '../../util';
-
 import styles from './TeacherInfobox.scss';
 
 const TeacherInfobox = React.createClass({
@@ -20,36 +18,34 @@ const TeacherInfobox = React.createClass({
     const teacherInfo = getTeacherInfo(teacherInfoContext);
 
     return (
-      <Collapse in={!this.props.isStudentMode}>
-        <Grid className={styles.infoBox} fluid={true}>
-          <Row className={styles.center}>
-            <Col xs={12} md={12}>
-                Du er nå lærermodus!
-                <br /><br />
-                Klikk på elev/lærer knappen i navigasjonsmenyen for å skifte modus.
-                Når du er i lærer-modus vil skoleemner ligge øverst i oppgavefilteret.
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={6} md={6}>
-              <h3>Lærer</h3>
-              {teacherInfo.teacher}
-            </Col>
-            <Col xs={6} md={6}>
-              <h3>Veileder</h3>
-              {teacherInfo.assistant}
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={6} md={6}>
-              <a className={styles.link} href={url[0]}>Lær mer</a>
-            </Col>
-            <Col xs={6} md={6}>
-              <a className={styles.link} href={url[1]}>Lær mer</a>
-            </Col>
-          </Row>
-        </Grid>
-      </Collapse>
+      <Grid className={styles.infoBox} fluid={true}>
+        <Row className={styles.center}>
+          <Col xs={12} md={12}>
+              Du er nå lærermodus!
+              <br /><br />
+              Klikk på elev/lærer knappen i navigasjonsmenyen for å skifte modus.
+              Når du er i lærer-modus vil skoleemner ligge øverst i oppgavefilteret.
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} md={6}>
+            <h3>Lærer</h3>
+            {teacherInfo.teacher}
+          </Col>
+          <Col xs={6} md={6}>
+            <h3>Veileder</h3>
+            {teacherInfo.assistant}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} md={6}>
+            <a className={styles.link} href={url[0]}>Lær mer</a>
+          </Col>
+          <Col xs={6} md={6}>
+            <a className={styles.link} href={url[1]}>Lær mer</a>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 
