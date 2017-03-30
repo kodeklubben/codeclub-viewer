8c907f3f83f319dd48b3861a191f4b79a13c063f
MrHanSan <hanssandbu@gmail.com>
2017-03-30 06:55:32 +0200

    212 filter .html away from links (#228)
    * Creates a function in utils for removing the ".html" ending for relative links to other lessons on the site, and uses the function in "Lesson.js"
    
    * Fixes linting
    
    * fixes comment in norwegian, changed to english
    
    * Fixes linting, one line was too long
    
    * Changes the edit function from while loop, to global regex match, and adds unit tests

--- src/util.js
+++ src/util.js
@@ -173,3 +173,9 @@ export function tagsMatchFilter(lessonTags, filter) {
 
   return true; // The lessonTags contained all the checked filterTags
 }
+
+export function removeHtmlFileEnding(lessonPage) {
+  // RegEx for matching and removing parts of text that starts with 
+  // <a href= ../ followed by anything not containing whitespaces, and ends with .html">
+  return lessonPage.replace(/(<a href="\.\.\/[^\s]*)\.html(">)/g, '$1$2');
+}
