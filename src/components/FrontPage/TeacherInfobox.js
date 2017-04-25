8929fed4beea23c26480d94becde95cb567af151
Skagevang <hero3@hotmail.com>
2017-04-25 17:18:37 +0200

    remove bootstrapgrid add collapsebutton

--- src/components/FrontPage/TeacherInfobox.js
+++ src/components/FrontPage/TeacherInfobox.js
@@ -1,13 +1,22 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import {getInfo} from '../../util';
 import styles from './TeacherInfobox.scss';
+import Button from 'react-bootstrap/lib/Button';
+import Collapse from 'react-bootstrap/lib/Collapse';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
 const TeacherInfobox = React.createClass({
+  
+  getInitialState() {
+   return {
+     showCourseInfo: false
+   };
+ },
+ changeState() {
+   this.setState({['showCourseInfo']: !this.state['showCourseInfo']});
+ },
 
   render() {
     const url = [
@@ -21,35 +30,32 @@ const TeacherInfobox = React.createClass({
 
     return (
       <div className={styles.center}>
-       {!this.props.isStudentMode ?
-        <Grid className={styles.infoBox} fluid={true}>
-          <Row>
-            <Col xs={12} md={12}>
-              <h3 className={styles.center}>{teacherInfo.welcomeTeacher[lang]}</h3>
-              <br />
-              {teacherInfo.changeModeTeacher[lang]}.
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={6} md={6}>
-              <h3>Lærer</h3>
-              {teacherInfo.teacher[lang]}
-            </Col>
-            <Col xs={6} md={6}>
-              <h3>Veileder</h3>
-              {teacherInfo.assistant[lang]}
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={6} md={6}>
-              <a className={styles.link} href={url[0]} target="_blank">{teacherInfo.link1[lang]}</a>
-            </Col>
-            <Col xs={6} md={6}>
-              <a className={styles.link} href={url[1]} target="_blank">{teacherInfo.link2[lang]}</a>
-            </Col>
-          </Row>
-        </Grid>
-        : null}
+        {!this.props.isStudentMode ?
+          <div className={styles.infoBox}>
+            <h3 className={styles.center}>{teacherInfo.welcomeTeacher[lang]}</h3>
+            <br />
+            {teacherInfo.changeModeTeacher[lang]}
+            <br />
+            <div className={styles.center}>
+              <Button className={styles.glyphicon} onClick={() => this.changeState()}>
+                <Glyphicon glyph={!this.state.showCourseInfo ? 'plus-sign' : 'minus-sign'}/>
+              </Button>
+            </div>
+            <Collapse in={this.state.showCourseInfo}>
+              <div>
+                <h3>{teacherInfo.teacherTitle[lang]}</h3>
+                {teacherInfo.teacher[lang]}
+                <br />
+                <a className={styles.link} href={url[0]} target="_blank">{teacherInfo.link1[lang]}</a>
+                <br />
+                <h3>{teacherInfo.assistantTitle[lang]}</h3>
+                {teacherInfo.assistant[lang]}
+                <br />
+                <a className={styles.link} href={url[1]} target="_blank">{teacherInfo.link2[lang]}</a>
+              </div>
+            </Collapse>
+          </div>
+          : null}
       </div>
     );
   }
