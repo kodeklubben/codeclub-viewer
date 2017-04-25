f35d336f6a0c8bef8a7263d7da350d514d38331c
krskog <k.skog@online.no>
2017-04-25 20:51:51 +0200

    Return text if index is empty

--- src/components/PlaylistPage/CourseInfo.js
+++ src/components/PlaylistPage/CourseInfo.js
@@ -6,13 +6,17 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './CourseInfo.scss';
 
 const CourseInfo = React.createClass({
-
   render() {
     const courseName = this.props.courseName;
     const indexfile = getCourseIndex(courseName);
+    const indexfileLength = indexfile.content.toString().length;
     return (      
       <div className={this.props.isStudentMode ? styles.containerStudent : styles.containerTeacher}>
-        <div dangerouslySetInnerHTML={{__html: indexfile.content}} />
+        {indexfileLength != 0 ?
+          <div dangerouslySetInnerHTML={{__html: indexfile.content}} />
+        :
+        <h4>Oops, her har noen glemt å skrive kursinformasjon!</h4>
+        }
       </div>
     );
   }
