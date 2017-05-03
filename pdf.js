4e525a64ec5629ff4468be23bd895df66c96e6c1
MrHanSan <hanssandbu@gmail.com>
2017-05-03 14:33:21 +0200

    Adds scratchblock generation to the pdf

--- pdf.js
+++ pdf.js
@@ -3,14 +3,15 @@ function buildPdf() {
   var dir = '../oppgaver/src/scratch/astrokatt/'
   var to = "./pdf/pdf.pdf"
   var cssPath = "./pdf/pdf.css"
-  var scratchblocks = require("scratchblocks/browser/scratchblocks.js")
   var markdownpdf = require("markdown-pdf")
     , fs = require("fs")
     , through = require("through")
     , split = require("split")
     , duplexer = require("duplexer")
-  let block = false;
-  let temp = "";
+  let block = 0;
+  let inBlock = false;
+  let temp = [];
+  //createDOM();
 
   var options = {
     cssPath: cssPath,
@@ -33,9 +34,10 @@ function buildPdf() {
     var splitter = split()
     var replacer = through(function (data) {
       //console.log(data)
-      if (data.match("```blocks") && block === false) {
-        temp = "{blocks}";
-        block = true;
+      if (data.match("```blocks") && inBlock === false) {
+        console.log("dataMatch");
+        temp[block] = "";
+        inBlock = true;
       } 
       else if (data.match(/---/)) {
         this.queue(data.replace(/---/, '') +'\n')
@@ -52,14 +54,19 @@ function buildPdf() {
       else if (data.match(/language:/)) {
         this.queue(data.replace(/(language: [a-zA-Z]*)/, '') +'\n')
       }
-      else if (block) {
+      /*else if (data.match(/\`[a-zA-Z\s]*\`\{\.block[a-z]*\}/)) {
+        console.log('replaceBlock')
+        this.queue(data.replace(/\`([a-zA-Z\s]*)\`\{\.(block[a-z]*)\}/g, '`{$2}$1`') +'\n')
+      }*/
+      else if (inBlock) {
         if (!data.match("```blocks") && data.match("```")) {
-          temp += data+"\n";
-          console.log(temp);
-          processContent(temp, jsonCSS);
-          block = false;
+          console.log("renderScratchBlocks")
+          //temp[block] += data+"\n";
+          this.queue(renderScratchBlocks(temp[block]));
+          block += 1;
+          inBlock = false;
         } else {
-          temp += data+"\n";
+          temp[block] += data+"\n";
         }
       } else {
         this.queue(data.replace(/({)\.([a-zA-Z]*})/g, '$1$2')+'\n')
@@ -71,19 +78,14 @@ function buildPdf() {
   
   function preProcessHTML () {
     var splitter = split()
+    block = 0;
     var replacer = through(function (data) {
-
-
-      /*if (data.match(/\`([a-zA-Z])\`\{\.block.*\}/)) {
-        this.queue(data.replace(/\`([a-zA-Z])\`\{\.(block.*\)})/, '<code class="$2">$1') +'</code>\n')
-      }*/
-
-      //console.log(hex2a(data.toString("hex")))
       console.log(data);
-      if (data.match(/<code>.*<\/code>{block[a-z]*}/)) {
-        //console.log('replace');
-        this.queue(data.replace(/(<code>)(.*)(<\/code>)\{(block[a-z]*)\}/, '<code class="$4">$2</code>')+'\n')
-      } else {
+      if (data.match(/<code>[a-zA-Z\s]*<\/code>\{block[a-z]*\}/g)) {
+        //console.log('replaceBlock');
+        this.queue(data.replace(/(<code>)([æøåÆØÅa-zA-Z\s]*)(<\/code>\{)(block[a-z]*)\}/g, '<code class="$4">$2</code>') + '\n');
+      }
+       else {
         this.queue(data);
       }
     })
@@ -92,107 +94,13 @@ function buildPdf() {
   }
 }
 
-function hex2a(hexx) {
-    var hex = hexx.toString();//force conversion
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
+function renderScratchBlocks(content) {
+  var scratchblocks = require('scratchblocks');
+  var fs = require('fs');
+
+  var svg = scratchblocks(content);
+
+  return svg;
 }
-
-
-
-
-const jsonCSS = {
-  "blocks": {
-    "background-color": "inherit",
-    "border": 0
-  },
-  "b": {
-    "background-color": "inherit",
-    "border": 0
-  }
-}
-
-function processContent(content, styles) {
-  const parser = require('posthtml-parser');
-  const render = require('posthtml-render');
-
-  let parsedContent = parser(content);
-  parsedContent = replaceClassRecursively(parsedContent, styles);
-  content = render(parsedContent);
-  content = renderScratchBlocks(content, styles);
-  return content;
-}
-
-function replaceTagObject(obj) {
-  /*const tag = obj['tag'];
-  if (tag in replaceTags) {
-    const replacementObj = replaceTags[tag];
-    return {
-      ...obj,
-      tag: replacementObj['tag'],
-      attrs: {...obj['attrs'], ...replacementObj['attrs']}
-    };
-  } else {
-    return obj;
-  }*/
-  return obj;
-}
-
-function replaceClassRecursively(obj, styles) {
-  if (Array.isArray(obj)) {
-    return obj.map((val, idx) => replaceClassRecursively(val, styles));
-  } else if (typeof obj === 'object' && obj !== null) {
-    const repObj = replaceTagObject(obj);
-    let newObj = {};
-    for (let k in repObj) {
-      if (repObj.hasOwnProperty(k)) {
-        if (k === 'class' && repObj[k] in styles) {
-          newObj[k] = styles[repObj[k]];
-        } else {
-          newObj[k] = replaceClassRecursively(repObj[k], styles);
-        }
-      }
-    }
-    return newObj;
-  } else {
-    return obj;
-  }
-}
-
-
-/**
- * Render scratchblocks.
- *
- * @param content {string} HTML with <pre class="blocks">...</pre>
- * @returns {string} <pre class="blocks">...</pre> replaced with SVG
- */
-function renderScratchBlocks(content, styles) {
-  let replace = [];
-  if ('blocks' in styles) {
-    replace.push({start: '<pre class="' + styles.blocks + '">', end: '</pre>'});
-  }
-  if ('b' in styles) {
-    replace.push({start: '<code class="' + styles.b + '">', end: '</code>', options: {inline: true}});
-  }
-
-  let returnContent = content;
-  replace.forEach(r => {
-    const re = new RegExp(r.start + '[\\s\\S]*?' + r.end, 'g');
-
-    let blocks = content.match(re);
-    if (blocks) {
-      blocks.forEach(block => {
-        let code = block.substring(r.start.length, block.length - r.end.length);
-        let SVG = scratchblocks(code, r.options);
-        returnContent = returnContent.replace(block, SVG);
-      });
-    }
-  });
-
-  return returnContent;
-}
-
 
 buildPdf();
