397cbb48c03361aa2ee100103089aea9f43f81c5
MrHanSan <hanssandbu@gmail.com>
2017-05-03 21:41:10 +0200

    commit before eol fix

--- pdf.js
+++ pdf.js
@@ -1,8 +1,6 @@
-function buildPdf() {
-  var oppg = '../oppgaver/src/scratch/astrokatt/astrokatt.md'
-  var dir = '../oppgaver/src/scratch/astrokatt/'
-  var to = "./pdf/pdf.pdf"
-  var cssPath = "./pdf/pdf.css"
+function buildPdf(dir) {
+  var oppg = getFileName(dir);
+  var cssPath = "./pdf/pdf.css";
   var markdownpdf = require("markdown-pdf")
     , fs = require("fs")
     , through = require("through")
@@ -11,7 +9,6 @@ function buildPdf() {
   let block = 0;
   let inBlock = false;
   let temp = [];
-  //createDOM();
 
   var options = {
     cssPath: cssPath,
@@ -26,24 +23,34 @@ function buildPdf() {
     }
   }
 
-  markdownpdf(options)
-    .from(oppg)
-    .to(to, function () { console.log("Done") })
+  for (o in oppg) {
+    //console.log(o)
+    if (typeof(oppg[o]) === "undefined") {
+      console.log(oppg[o])
+    }
+    else {
+      var to = "./pdf/test/" + oppg[o].split(".")[0] + ".pdf";
+      console.log(dir+"/"+oppg[o])
+      markdownpdf(options)
+        .from(dir+"/"+oppg[o])
+        .to(to, function () { console.log("Done") })
+      }
+    }
 
   function preProcessMD () {
     var splitter = split()
     var replacer = through(function (data) {
-      //console.log(data)
+      console.log(data)
       if (data.match("```blocks") && inBlock === false) {
         console.log("dataMatch");
         temp[block] = "";
         inBlock = true;
-      } 
+      }
       else if (data.match(/---/)) {
         this.queue(data.replace(/---/, '') +'\n')
       }
       else if (data.match(/title: /)) {
-        this.queue(data.replace(/(title: )([a-zA-Z])/, '$2') +'{title}\n\n')
+        this.queue(data.replace(/(title: )([æøåÆØÅa-zA-Z])/, '$2') +'{title}\n\n')
       } 
       else if (data.match(/level: /)) {
         this.queue(data.replace(/(level: )([0-9])/, 'Level $2{icon-$2}') +'\n\n')
@@ -54,10 +61,6 @@ function buildPdf() {
       else if (data.match(/language:/)) {
         this.queue(data.replace(/(language: [a-zA-Z]*)/, '') +'\n')
       }
-      /*else if (data.match(/\`[a-zA-Z\s]*\`\{\.block[a-z]*\}/)) {
-        console.log('replaceBlock')
-        this.queue(data.replace(/\`([a-zA-Z\s]*)\`\{\.(block[a-z]*)\}/g, '`{$2}$1`') +'\n')
-      }*/
       else if (inBlock) {
         if (!data.match("```blocks") && data.match("```")) {
           console.log("renderScratchBlocks")
@@ -68,8 +71,18 @@ function buildPdf() {
         } else {
           temp[block] += data+"\n";
         }
-      } else {
+      }
+      else if (data.match(/\{\.protip\}/)) {
+        this.queue(data.replace(/\{.protip\}/, "")+"\n")
+      }
+      else if (data.match(/\{\.check\}/)) {
+	      this.queue(data.replace(/\{\.check\}/, "")+'\n');
+      }
+      else if (data.match(/({)\.([a-zA-Z]*})/)) {
         this.queue(data.replace(/({)\.([a-zA-Z]*})/g, '$1$2')+'\n')
+      }
+      else {
+        this.queue(data+'\n');
       }
     })
     splitter.pipe(replacer)
@@ -80,9 +93,9 @@ function buildPdf() {
     var splitter = split()
     block = 0;
     var replacer = through(function (data) {
-      console.log(data);
-      if (data.match(/<code>[a-zA-Z\s]*<\/code>\{block[a-z]*\}/g)) {
-        //console.log('replaceBlock');
+      //console.log(data);
+      if (data.match(/<code>[æøåÆØÅa-zA-Z\s]*<\/code>\{block[a-z]*\}/g)) {
+        console.log(data);
         this.queue(data.replace(/(<code>)([æøåÆØÅa-zA-Z\s]*)(<\/code>\{)(block[a-z]*)\}/g, '<code class="$4">$2</code>') + '\n');
       }
        else {
@@ -96,11 +109,38 @@ function buildPdf() {
 
 function renderScratchBlocks(content) {
   var scratchblocks = require('scratchblocks');
-  var fs = require('fs');
-
   var svg = scratchblocks(content);
 
   return svg;
 }
 
-buildPdf();
+function init() {
+  const rootDir = "../oppgaver/src/"
+  const dirs = getDirectories(rootDir);
+
+  for (d in dirs) {
+    subDirs = getDirectories(rootDir+dirs[d])
+    for (sd in subDirs){
+      buildPdf(rootDir+dirs[d]+"/"+subDirs[sd]);
+      //console.log(dirs[d]+"/"+subDirs[sd]);
+    }
+  }
+}
+
+function getDirectories (rootDir) {
+  const fs = require('fs')
+  const path = require('path')
+  return fs.readdirSync(rootDir)
+    .filter(file => fs.statSync(path.join(rootDir, file)).isDirectory())
+}
+
+function getFileName(dir) {
+  let subDirName = dir.split("/").pop().toLowerCase();
+  const re = new RegExp(subDirName + "[0-9]*\.md")
+  const fs = require('fs');
+
+  return fs.readdirSync(dir)
+    .filter(file => file.toLowerCase().match(re))
+}
+
+init();
