7451f4d16f3a2e7069c96717c468bcbdeffa9ae7
krskog <k.skog@online.no>
2017-05-03 23:25:19 +0200

    More work on caption translation

--- src/util.js
+++ src/util.js
@@ -106,6 +106,45 @@ export function funky(language, captionID, level) {
   return file[language][captionID];
 }
 
+
+/**
+ * @param captionPath (string) A dot-separated path to the caption, e.g. 'myPage.firstPanel.btnText'
+ * @param replacements (object) Key-value pairs of replacements in the caption,
+ *   e.g. cap='{{one}} or {{two}}' and replacement={one:'cat', two:'dog'} gives cap='cat or dog'
+ * @returns {*}
+ */
+
+export function translate(captionPath, replacements) {
+  const captions = require('./constants/captions_nb.js').default;
+  const caps = captions;
+  if (!captionPath || (typeof captionPath !== 'string')) {
+    console.error(`ERROR: path should be string (was ${typeof captionPath})`);
+    return null;
+  }
+  const keyArr = captionPath.split('.');
+  let cap = caps;
+  for(let key of keyArr) {
+    if (cap) {
+      cap = cap[key];
+    }
+  }
+  if (typeof cap !== 'string' && typeof cap !== 'number') {
+    // Non-existent path, return caption=captionPath
+    cap = captionPath;
+  }
+
+  // Replace {{key}} with replacements[key] in caption
+  if (cap && replacements) {
+    for (let repKey in replacements) {
+      if (replacements.hasOwnProperty(repKey)) {
+        const pattern = new RegExp('\\\{\\\{' + repKey + '\\\}\\\}', 'gm'); // g=global, m=multiline
+        cap = cap.replace(pattern, replacements[repKey]);
+      }
+    }
+  }
+  return cap;
+}
+
 ///////////////////////////////////
 //////// HELPER FUNCTIONS /////////
 ///////////////////////////////////
