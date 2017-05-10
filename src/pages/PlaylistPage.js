e9a4b2821e43991402d11c28da82983324579f86
agnete <agnetedjupvik@gmail.com>
2017-05-10 15:33:38 +0200

    Display PlaylistPage components in a DRY fashion

--- src/pages/PlaylistPage.js
+++ src/pages/PlaylistPage.js
@@ -53,6 +53,23 @@ export const PlaylistPage = React.createClass({
       </div>
     ));
     const showLevelNavigationDesktop = Object.keys(lessons).length > 15 && levels.length > 1;
+
+    const filter =
+      <div className={styles.filter}>
+        <Filter isStudentMode={this.props.isStudentMode}/>
+      </div>;
+
+    const playlistsAndLessons =
+      <div>
+        <PlaylistNavigation playlists={playlists}/>
+        {lessonLists.length ? lessonLists : 'Ingen oppgaver passer til filteret'}
+      </div>;
+
+    const jumpTo =
+      <div className={styles.scrollable}>
+        {showLevelNavigationDesktop ? <LevelNavigation levels={levels}/> : null}
+      </div>;
+
     return (
       <Grid fluid={true}>
 
@@ -62,50 +79,17 @@ export const PlaylistPage = React.createClass({
         <Row>
           {/*Filter desktop*/}
           <Col xsHidden>
-            <Col sm={3}>
-              <div className={styles.filter}>
-                <Filter isStudentMode={this.props.isStudentMode}/>
-              </div>
-            </Col>
-
-
-            <Col sm={6}>
-              {/*Desktop playlists*/}
-              <PlaylistNavigation playlists={playlists}/>
-              {/*List of lessons grouped by level*/}
-              {lessonLists.length ? lessonLists : 'Ingen oppgaver passer til filteret'}
-            </Col>
-
-            <Col sm={3}>
-              <div className={styles.scrollable}>
-                {/*Desktop level navigation*/}
-                {showLevelNavigationDesktop ? <LevelNavigation levels={levels}/> : null}
-              </div>
-            </Col>
+            <Col sm={3}>{filter}</Col>
+            <Col sm={6}>{playlistsAndLessons}</Col>
+            <Col sm={3}>{jumpTo}</Col>
           </Col>
 
           {/*Filter mobile*/}
           <Col smHidden mdHidden lgHidden>
-            <Col xs={12}>
-              <div className={styles.filter}>
-                <Filter isStudentMode={this.props.isStudentMode}/>
-              </div>
-            </Col>
-
-            <Col xs={12}>
-              <div className={styles.scrollable}>
-                {showLevelNavigationDesktop ? <LevelNavigation levels={levels}/> : null}
-              </div>
-            </Col>
-
-            <Col xs={12}>
-              <PlaylistNavigation playlists={playlists}/>
-              {/*List of lessons grouped by level*/}
-              {lessonLists.length ? lessonLists : 'Ingen oppgaver passer til filteret'}
-            </Col>
-
+            <Col xs={12}>{filter}</Col>
+            <Col xs={12}>{jumpTo}</Col>
+            <Col xs={12}>{playlistsAndLessons}</Col>
           </Col>
-
         </Row>
       </Grid>
     );
