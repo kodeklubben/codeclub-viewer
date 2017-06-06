0814ba20992ee12bb0b3e79ceab03ef33c11d709
Skagevang <hero3@hotmail.com>
2017-06-06 11:05:28 +0200

    fixed startButton in WelcomeBox

--- src/components/FrontPage/WelcomeBox.js
+++ src/components/FrontPage/WelcomeBox.js
@@ -8,49 +8,33 @@ import {getTranslator} from '../../selectors/translate';
 import LinkContainer from 'react-router-bootstrap/lib/LinkContainer';
 import {setWelcomeBox} from '../../action_creators';
 
-const WelcomeBox = React.createClass({
+const WelcomeBox = ({t, welcomeBox, setWelcomeBox}) => {
+  const startButton = <div className={styles.center}>
+    <LinkContainer to={t('frontpage.welcomebox.buttonlink')}>
+      <Button bsStyle='student-frontpage'>
+        {t('frontpage.welcomebox.startbutton')}
+      </Button>
+    </LinkContainer>
+  </div>;
 
-  render() {
-    const {t, welcomeBox, setWelcomeBox} = this.props;
-
-    if(welcomeBox) {
-      return (
-        <div className={styles.center}>
-          <div className={styles.infoBox}>
-            <h3 className={styles.center}>
-            {t('frontpage.welcomebox.header')}</h3>
-            <Button className={styles.xSign} onClick={() => setWelcomeBox(false)}>
-              <Glyphicon glyph="remove"/>
-            </Button>
-            <br />
-            {t('frontpage.welcomebox.changemode')}
-            <br /><br />
-            {t('frontpage.welcomebox.info')}
-            <br /><br />
-            <div className={styles.center}>
-              <LinkContainer to={t('frontpage.welcomebox.buttonlink')}>
-                <Button bsStyle='student-frontpage'>
-                  {t('frontpage.welcomebox.startbutton')}
-                </Button>
-              </LinkContainer>
-            </div>
-          </div>
-        </div>
-      );
-    }
-    else {
-      return (
-      <div className={styles.center}>
-       <LinkContainer to={t('frontpage.welcomebox.buttonlink')}>
-         <Button bsStyle='student-frontpage'>
-           {t('frontpage.welcomebox.startbutton')}
-         </Button>
-       </LinkContainer>
-      </div>);
-    }
-  }
-
-});
+  return welcomeBox ?
+    <div className={styles.center}>
+      <div className={styles.infoBox}>
+        <h3 className={styles.center}>
+          {t('frontpage.welcomebox.header')}</h3>
+        <Button className={styles.xSign} onClick={() => setWelcomeBox(false)}>
+          <Glyphicon glyph="remove"/>
+        </Button>
+        <br />
+        {t('frontpage.welcomebox.changemode')}
+        <br /><br />
+        {t('frontpage.welcomebox.info')}
+        <br /><br />
+        {startButton}
+      </div>
+    </div> :
+    startButton;
+};
 
 WelcomeBox.propTypes = {
   t: PropTypes.func,
