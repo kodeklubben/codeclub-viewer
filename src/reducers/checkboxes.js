61da35e36e13aa27b9ac96f5da1244c3864a4470
Ole-Martin Skagevang <hero3@hotmail.com>
2017-06-23 19:54:13 +0200

    less code in reducer

--- src/reducers/checkboxes.js
+++ src/reducers/checkboxes.js
@@ -1,11 +1,10 @@
 import {localstorageStoreCheckboxes} from '../localStorage';
 
 export default function(state={}, action) {
-  let checkboxObject;
   switch(action.type) {
     case 'SET_CHECKBOXES':
-      checkboxObject = localstorageStoreCheckboxes(action.payload.path, action.payload.checkboxes);
-      return {...state, [action.payload.path]: checkboxObject };
+      return {...state,
+        [action.payload.path]: localstorageStoreCheckboxes(action.payload.path, action.payload.checkboxes) };
   }
   return state;
 }
