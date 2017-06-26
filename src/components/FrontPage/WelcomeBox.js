7ce91d4041a16d44c54b5c3fcf8df539574db36f
Ole-Martin Skagevang <hero3@hotmail.com>
2017-06-26 22:37:01 +0200

    fixed last lesson for welcomebox

--- src/components/FrontPage/WelcomeBox.js
+++ src/components/FrontPage/WelcomeBox.js
@@ -8,13 +8,21 @@ import {getTranslator} from '../../selectors/translate';
 import LinkContainer from 'react-router-bootstrap/lib/LinkContainer';
 import {setWelcomeBox} from '../../action_creators';
 
-const WelcomeBox = ({t, welcomeBox, setWelcomeBox}) => {
+const WelcomeBox = ({t, welcomeBox, setWelcomeBox, checkboxes}) => {
+  const checkboxKeys = Object.keys(checkboxes);
   const startButton = <div className={styles.center}>
+    {checkboxKeys.length === 0 ?
     <LinkContainer to={t('frontpage.welcomebox.buttonlink')}>
       <Button bsStyle='student-frontpage'>
         {t('frontpage.welcomebox.startbutton')}
       </Button>
     </LinkContainer>
+    :
+    <LinkContainer to={checkboxKeys[checkboxKeys.length - 1]}>
+      <Button bsStyle='student-frontpage'>
+        {t('frontpage.welcomebox.continuebutton')}
+      </Button>
+    </LinkContainer>}
   </div>;
 
   return welcomeBox ?
@@ -39,12 +47,14 @@ const WelcomeBox = ({t, welcomeBox, setWelcomeBox}) => {
 WelcomeBox.propTypes = {
   t: PropTypes.func,
   welcomeBox: PropTypes.bool,
-  setWelcomeBox: PropTypes.func
+  setWelcomeBox: PropTypes.func,
+  checkboxes: PropTypes.object
 };
 
 const mapStateToProps = (state) => ({
   t: getTranslator(state),
-  welcomeBox: state.welcomeBox
+  welcomeBox: state.welcomeBox,
+  checkboxes: state.checkboxes
 });
 
 
