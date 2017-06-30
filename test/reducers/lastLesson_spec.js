0e33ac287dbd7530ebc2440900209f2e32e3e424
Ole-Martin Skagevang <hero3@hotmail.com>
2017-06-30 11:19:40 +0200

    more tests

--- test/reducers/lastLesson_spec.js
+++ test/reducers/lastLesson_spec.js
@@ -5,32 +5,86 @@ import reducer from '../../src/reducers/lastLesson';
 
 describe('lastLesson reducer', () => {
   describe('SET_LASTLESSON', () => {
-    it('Sets state to empty string', () => {
+    it('Sets last lesson in state to empty string', () => {
       const initialState = '';
       const action = {
         type: 'SET_LASTLESSON',
-        path: initialState
+        path: ''
       };
 
       deepFreeze(initialState);
       deepFreeze(action);
       const nextState = reducer(initialState, action);
 
-      expect(nextState).to.equal('');
+      expect(nextState).to.equal(action.path);
     });
 
     it('Sets last lesson in state to /scratch/astrokatt/astrokatt', () => {
-      const initialState = '/scratch/astrokatt/astrokatt';
+      const initialState = '';
       const action = {
         type: 'SET_LASTLESSON',
-        path: initialState
+        path: '/scratch/astrokatt/astrokatt'
       };
 
       deepFreeze(initialState);
       deepFreeze(action);
       const nextState = reducer(initialState, action);
 
-      expect(nextState).to.equal('/scratch/astrokatt/astrokatt');
+      expect(nextState).to.equal(action.path);
+    });
+
+    it('Missing type', () => {
+      const initialState = '';
+      const action = {
+        path: '/scratch/astrokatt/astrokatt'
+      };
+
+      deepFreeze(initialState);
+      deepFreeze(action);
+      const nextState = reducer(initialState, action);
+
+      expect(nextState).to.equal(action.path);
+    });
+
+    it('Type unknown', () => {
+      const initialState = '';
+      const action = {
+        type: 'unknown',
+        path: '/scratch/astrokatt/astrokatt'
+      };
+
+      deepFreeze(initialState);
+      deepFreeze(action);
+      const nextState = reducer(initialState, action);
+
+      expect(nextState).to.equal(action.path);
+    });
+
+    it('Missing path', () => {
+      const initialState = '';
+      const action = {
+        type: 'SET_LASTLESSON'
+      };
+
+      deepFreeze(initialState);
+      deepFreeze(action);
+      const nextState = reducer(initialState, action);
+
+      expect(nextState).to.equal(action.path);
+    });
+
+    it('Path unknown', () => {
+      const initialState = '';
+      const action = {
+        type: 'SET_LASTLESSON',
+        path: 'unknown'
+      };
+
+      deepFreeze(initialState);
+      deepFreeze(action);
+      const nextState = reducer(initialState, action);
+
+      expect(nextState).to.equal(action.path);
     });
   });
 });
