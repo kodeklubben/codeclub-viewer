a714ac3ce125a8b98106399964c70ffc581db7dc
Ole-Martin Skagevang <hero3@hotmail.com>
2017-07-21 10:59:16 +0200

    removed if condition

--- src/util.js
+++ src/util.js
@@ -61,12 +61,10 @@ export function getLessons(lessonContext, readmeContext, courseContext) {
     const language = lessonFrontmatter.language;
 
     if (language){
-      if (lessonFrontmatter.indexed !== false) {
-        if (lessonFrontmatter.tags) {
-          lessonFrontmatter.tags['language'] = language;
-        } else {
-          lessonFrontmatter.tags = {language: [language]};
-        }
+      if (lessonFrontmatter.tags) {
+        lessonFrontmatter.tags['language'] = language;
+      } else {
+        lessonFrontmatter.tags = {language: [language]};
       }
     }
 
