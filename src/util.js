95882d3fcc4a9317b2339012eef1c0f708beff29
Ole-Martin Skagevang <hero3@hotmail.com>
2017-07-21 19:38:54 +0200

    Issue 316 removed Croatia (#321)
    * removed croatia
    
    * fix for lessons that is indexed false
    
    * removed if condition

--- src/util.js
+++ src/util.js
@@ -60,10 +60,10 @@ export function getLessons(lessonContext, readmeContext, courseContext) {
 
     const language = lessonFrontmatter.language;
 
-    if(language){
-      if(lessonFrontmatter.tags){
+    if (language){
+      if (lessonFrontmatter.tags) {
         lessonFrontmatter.tags['language'] = language;
-      }else{
+      } else {
         lessonFrontmatter.tags = {language: [language]};
       }
     }
@@ -269,7 +269,7 @@ export function removeHtmlFileEnding(lessonPage) {
 * All available languages must be defined here
 * @returns {Array} An array of available languages
 */
-export const getAvailableLanguages = () => ['nb', 'nn', 'sv', 'da', 'en', 'hr'];
+export const getAvailableLanguages = () => ['nb', 'nn', 'sv', 'da', 'en'];
 
 /**
 * Returns groupNames with tags that should be considered as logical OR in the filter.
