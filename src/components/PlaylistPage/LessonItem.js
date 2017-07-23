0ba2d5f45f95593b13a8711f9b11476fbacfdebd
Lance Olav Eastgate <lanceolav@lanceolav.com>
2017-07-23 20:42:57 +0200

    Create cached selectors (and unit tests) for getNumberOfCheckedCheckboxes and getTotalNumberOfCheckboxes.

--- src/components/PlaylistPage/LessonItem.js
+++ src/components/PlaylistPage/LessonItem.js
@@ -10,104 +10,80 @@ import Tooltip from 'react-bootstrap/lib/Tooltip';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import LevelIcon from '../LevelIcon';
 import {getTranslator} from '../../selectors/translate';
-import {getLessonIntro} from '../../util';
+import {getLessonIntro, createCheckboxesKey} from '../../util';
+import {getNumberOfCheckedCheckboxes, getTotalNumberOfCheckboxes} from '../../selectors/checkboxes';
 
-const percentObject = {};
-const getCheckboxProgress = (path, checkboxes) => {
-  let checkboxKeys = Object.keys(checkboxes);
-  let trueCheckboxes = 0;
-  let checkboxObject = {};
-  let value = false;
-  for (let i = 0; i < checkboxKeys.length; i++) {
-    checkboxObject = checkboxes[checkboxKeys[i]];
-    checkboxKeys[i] = checkboxKeys[i].replace('checkboxes_','/');
-    if (checkboxKeys[i] === path) {
-      for (let key in checkboxObject) {
-        value = checkboxObject[key];
-        if (value === true) {
-          trueCheckboxes++;
-        }
-      }
-      percentObject[path] = (trueCheckboxes/Object.keys(checkboxObject).length)*100;
-      return '(' + trueCheckboxes + '/' + Object.keys(checkboxObject).length + ')';
-    }
-  }
-};
+export const LessonItem = ({t, lesson, isStudentMode, checkedCheckboxes, totalCheckboxes}) => {
+  const levelIcon = <LevelIcon level={lesson.level}/>;
 
-const lessonColor = (level) => {
-  if (level === 1) {
-    return '#46cc46';
-  }
-  else if (level === 2) {
+  const progressPercent = totalCheckboxes > 0 ? 100 * checkedCheckboxes / totalCheckboxes : 0;
+  const progress = checkedCheckboxes > 0 ?
+    <div className={styles.progress}>
+      {`(${checkedCheckboxes}/${totalCheckboxes})`}
+    </div> :
+    null;
 
-    return '#368BD8';
-  }
-  else if (level === 3) {
-    return '#D63838';
-  }
-  else if (level === 4) {
-    return '#333333';
-  }
-};
+  const instructionBtn = !isStudentMode && lesson.readmePath ?
+    <LinkContainer to={lesson.readmePath}>
+      <Button componentClass="div" className={styles.instructionBtn} bsStyle="guide" bsSize="xs">
+        {t('playlist.instructionbutton')}
+      </Button>
+    </LinkContainer>
+    : null;
 
-export const LessonItem = React.createClass({
-  render() {
-    const {t, lesson, isStudentMode, checkboxes} = this.props;
-    const levelIcon = <LevelIcon level={lesson.level}/>;
-    const progress = <div className={styles.progress}>
-      {getCheckboxProgress(lesson.path, checkboxes)}</div>;
-    const instructionBtn = !isStudentMode && lesson.readmePath ?
-      <LinkContainer to={lesson.readmePath}>
-        <Button componentClass="div" className={styles.instructionBtn} bsStyle="guide" bsSize="xs">
-          {t('playlist.instructionbutton')}
-        </Button>
-      </LinkContainer>
-      : null;
-    const tooltipContent = getLessonIntro(lesson.path.slice(1));
-    const createMarkup = () => {
-      return {__html: tooltipContent};
-    };
-    const tooltip =
-      <Tooltip className={styles.tooltip} id={lesson.title}>
-        <div dangerouslySetInnerHTML={createMarkup()}/>
-      </Tooltip>;
-    return (
-      <OverlayTrigger animation={true} delayShow={400} placement="bottom" overlay={tooltip}>
-        {lesson.external ?
-        <ListGroupItem href={lesson.external} target="_blank" className={styles.row}>
+  const tooltipContent = getLessonIntro(lesson.path.slice(1));
+
+  const createMarkup = () => {
+    return {__html: tooltipContent};
+  };
+
+  const tooltip =
+    <Tooltip className={styles.tooltip} id={lesson.title}>
+      <div dangerouslySetInnerHTML={createMarkup()}/>
+    </Tooltip>;
+
+  const progressBar = lesson.level > 0 ?
+    <span className={styles['progressBarLevel' + lesson.level]} style={{width: progressPercent + '%'}}/> :
+    null;
+
+  return (
+    <OverlayTrigger animation={true} delayShow={400} placement="bottom" overlay={tooltip}>
+      {lesson.external ?
+      <ListGroupItem href={lesson.external} target="_blank" className={styles.row}>
+        {levelIcon}
+        <div className={styles.title}>{lesson.title}</div>
+        &nbsp;<Glyphicon glyph="new-window"/>
+        {instructionBtn}
+      </ListGroupItem>
+      :
+      <LinkContainer to={lesson.path}>
+        <ListGroupItem className={styles.row}>
+          {progressBar}
           {levelIcon}
           <div className={styles.title}>{lesson.title}</div>
-          &nbsp;<Glyphicon glyph="new-window"/>
+          {progress}
           {instructionBtn}
         </ListGroupItem>
-        :
-        <LinkContainer to={lesson.path}>
-          <ListGroupItem className={styles.row}>
-            <span className={styles.progressBar}
-              style={{width: percentObject[lesson.path] + '%', backgroundColor: lessonColor(lesson.level)}}></span>
-            {levelIcon}
-            <div className={styles.title}>{lesson.title}</div>
-            {progress}
-            {instructionBtn}
-          </ListGroupItem>
-        </LinkContainer>}
-      </OverlayTrigger>
-    );
-  }
-});
+      </LinkContainer>}
+    </OverlayTrigger>
+  );
+};
 
 LessonItem.propTypes = {
   lesson: PropTypes.object,
   isStudentMode: PropTypes.bool,
   t: PropTypes.func,
-  checkboxes: PropTypes.object
+  checkedCheckboxes: PropTypes.number.isRequired,
+  totalCheckboxes: PropTypes.number.isRequired,
 };
 
-function mapStateToProps(state) {
+function mapStateToProps(state, ownProps) {
+  const lessonCheckboxesKey = createCheckboxesKey(ownProps.lesson.path);
   return {
     isStudentMode: state.isStudentMode,
     t: getTranslator(state),
-    checkboxes: state.checkboxes
+    checkedCheckboxes: getNumberOfCheckedCheckboxes(state, lessonCheckboxesKey),
+    totalCheckboxes: getTotalNumberOfCheckboxes(state, lessonCheckboxesKey),
   };
 }
 
