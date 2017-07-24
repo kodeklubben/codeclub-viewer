90744b9e2b60bd4fd5b504d10644af65ac0dfa4f
Lance Olav Eastgate <lanceolav@lanceolav.com>
2017-07-24 18:47:40 +0200

    Change courseinfobutton and courseinfo into a collapsible panel.

--- src/components/PlaylistPage/CourseInfo.js
+++ src/components/PlaylistPage/CourseInfo.js
@@ -1,31 +1,35 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
+import Panel from 'react-bootstrap/lib/Panel';
+
 import {getCourseInfoMarkup} from '../../util';
 import {getTranslator} from '../../selectors/translate';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import styles from './CourseInfo.scss';
 
-const CourseInfo = React.createClass({
-  render() {
-    const {t, isStudentMode, courseInfo} = this.props;
-    return (
-      <div className={isStudentMode ? styles.containerStudent : styles.containerTeacher}>
+const CourseInfo = ({t, isStudentMode, courseInfo}) => {
+  const bsStyle = isStudentMode ? 'student' : 'teacher';
+  return (
+    <Panel collapsible defaultExpanded={false} bsStyle={bsStyle} header={t('playlist.courseinfo')}>
         {courseInfo ?
           <div dangerouslySetInnerHTML={courseInfo} />
         :
           <h4>{t('playlist.courseinfonotfound')}</h4>
         }
-      </div>
-    );
-  }
-});
-
-CourseInfo.propTypes = {
-  isStudentMode: PropTypes.bool,
-  t: PropTypes.func,
-  courseName: PropTypes.string
+    </Panel>
+  );
 };
 
+CourseInfo.propTypes = {
+  // ownProps:
+  isStudentMode: PropTypes.bool,
+
+  // mapStateToProps:
+  t: PropTypes.func,
+  courseInfo: PropTypes.object
+};
+
+/**
+ * Input props: courseName
+ */
 function mapStateToProps(state, props) {
   return {
     t: getTranslator(state),
@@ -33,4 +37,4 @@ function mapStateToProps(state, props) {
   };
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(CourseInfo));
+export default connect(mapStateToProps)(CourseInfo);
