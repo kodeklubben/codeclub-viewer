503a423065a98a97ba1067cb85f7e1b17724ece7
Lance Olav Eastgate <lanceolav@lanceolav.com>
2017-07-28 13:40:08 +0200

    Use Panel for LevelNavigation, and some small fixes

--- src/components/PlaylistPage/LevelNavigation.js
+++ src/components/PlaylistPage/LevelNavigation.js
@@ -1,28 +1,31 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
-import {getTranslator} from '../../selectors/translate';
-import LevelIcon from '../LevelIcon';
+import Panel from 'react-bootstrap/lib/Panel';
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './LevelNavigation.scss';
+import {getTranslator} from '../../selectors/translate';
+import LevelIcon from '../LevelIcon';
+
 
 const LevelNavigation = ({t, levels}) => {
   const levelListItems = levels.map((level, idx) => (
-    <ListGroupItem key={idx} onClick={() => document.getElementById('level-' + level).scrollIntoView()}>
+    <ListGroupItem className={styles.listGroupItem}
+                   key={idx}
+                   onClick={() => document.getElementById('level-' + level).scrollIntoView()}>
       <LevelIcon level={level}/>{t('general.levels.' + level)}
     </ListGroupItem>
   ));
+  const header = <span className={styles.header}>{t('playlist.levelnavigation')}</span>;
   return (
-    <div>
-      <h3>{t('playlist.levelnavigation')}</h3>
-      <div style={{fontSize: '1.25em'}}>
-        <ListGroup>
-          {levelListItems}
-        </ListGroup>
-      </div>
-    </div>
+    <Panel header={<div>{header}</div>}>
+      <ListGroup fill>
+        {levelListItems}
+      </ListGroup>
+    </Panel>
   );
 };
-
 
 LevelNavigation.propTypes = {
   levels: PropTypes.array,
@@ -35,4 +38,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(LevelNavigation);
+export default connect(mapStateToProps)(withStyles(styles)(LevelNavigation));
