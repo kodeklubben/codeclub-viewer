0f60300a6fb451c2514b4ad3b4e2b338e23e852c
Ole-Martin Skagevang <hero3@hotmail.com>
2017-08-01 22:14:04 +0200

    Issue 364 CollapsiblePanel for filter (#373)
    * allmost finished. removed filterbutton
    
    * add tooltip
    
    * removed alot from filter.js
    
    * cleanup
    
    * some more cleanup
    
    * fixed props
    
    * removed one last prop
    
    * cr

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -3,33 +3,28 @@ import {connect} from 'react-redux';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import Filter from '../components/FrontPage/Filter';
+import LessonFilter from '../components/Filter/LessonFilter';
 import {CoursesContainer} from '../components/FrontPage/Courses';
 import TeacherInfobox from '../components/FrontPage/TeacherInfobox';
 import WelcomeBox from '../components/FrontPage/WelcomeBox';
 
-export const  FrontPage = React.createClass({
+export const  FrontPage = ({isStudentMode}) => {
+  return (
+    <Grid fluid={true}>
+      {/*WelcomeBox and TeacherInfobox*/}
+      {isStudentMode ? <WelcomeBox /> : <TeacherInfobox />}
 
-  render() {
-    const {isStudentMode} = this.props;
-    return (
-      <Grid fluid={true}>
-        {/*WelcomeBox and TeacherInfobox*/}
-        {isStudentMode ? <WelcomeBox /> : <TeacherInfobox />}
-        
-        <hr/>
-        {/* Filter and courses */}
-        <Row>
-          <Col sm={4} md={3} lg={2}>
-            <Filter isStudentMode={isStudentMode}/>
-          </Col>
-          <CoursesContainer/>
-        </Row>
-      </Grid>
-    );
-  }
-
-});
+      <hr/>
+      {/* Filter and courses */}
+      <Row>
+        <Col sm={4} md={3} lg={2}>
+          <LessonFilter/>
+        </Col>
+        <CoursesContainer/>
+      </Row>
+    </Grid>
+  );
+};
 
 FrontPage.propTypes = {
   isStudentMode: PropTypes.bool
