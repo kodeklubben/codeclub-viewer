381d41c3900aeab318f787d9abadf625a517db6d
Ole-Martin Skagevang <hero3@hotmail.com>
2017-08-14 12:27:38 +0200

    fixed point 3

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -6,44 +6,37 @@ import Row from 'react-bootstrap/lib/Row';
 import {getTranslator} from '../../selectors/translate';
 import styles from './Footer.scss';
 
-const Footer = React.createClass({
-  getGitHubLink() {
-    const url = {
-      oppgaver: 'https://github.com/kodeklubben/oppgaver/'
-    };
+const Footer = ({t, isStudentMode}) => {
+  const url = {
+    oppgaver: 'https://github.com/kodeklubben/oppgaver/',
+    kidsakoder: 'http://kidsakoder.no/',
+    archive: 'https://github.com/kodeklubben/kodeklubben.github.io/archive/master.zip',
+    sponsor: 'http://kidsakoder.no/2015/07/03/kodeklubben-trondheim-utvikler-materiell-i-sommer/',
+    excited: 'http://www.ntnu.edu/web/excited',
+    ntnu_idi: 'https://www.ntnu.edu/idi/',
+    uio_ifi: 'http://www.mn.uio.no/ifi/',
+    teknograd: 'https://www.teknograd.no/'
+  };
 
-    return (
-      <div>
-        <Row className={styles.center}>
-          <p>Bidra?</p>
-        </Row>
-        <Row className={styles.center}>
-          <a href={url.oppgaver}>
-            <img className={styles.svg} src={require('../../assets/graphics/github-teacher.svg')}/>
-          </a>
-        </Row>
-        <Row>
-          <div className={styles.divider}/>
-        </Row>
-      </div>
-    );
-  },
-
-  render() {
-    const {t, isStudentMode} = this.props;
-    const url = {
-      kidsakoder: 'http://kidsakoder.no/',
-      archive: 'https://github.com/kodeklubben/kodeklubben.github.io/archive/master.zip',
-      sponsor: 'http://kidsakoder.no/2015/07/03/kodeklubben-trondheim-utvikler-materiell-i-sommer/',
-      excited: 'http://www.ntnu.edu/web/excited',
-      ntnu_idi: 'https://www.ntnu.edu/idi/',
-      uio_ifi: 'http://www.mn.uio.no/ifi/',
-      teknograd: 'https://www.teknograd.no/'
-    };
-
-    return (
+  return (
+    <div>
       <Grid fluid={true} className={isStudentMode ? styles.containerStudent : styles.containerTeacher}>
-        {isStudentMode ? null : this.getGitHubLink()}
+
+      {isStudentMode ? null :
+        <div>
+          <Row className={styles.center}>
+            <p>{t('footer.contribute')}</p>
+          </Row>
+          <Row className={styles.center}>
+            <a href={url.oppgaver}>
+              <img className={styles.svg} src={require('../../assets/graphics/github-teacher.svg')}/>
+            </a>
+          </Row>
+          <Row>
+            <div className={styles.divider}/>
+          </Row>
+        </div>}
+        
         <Row className={styles.center}>
             <a className={styles.inline} href={url.kidsakoder}>kidsakoder.no</a>
             <a className={styles.inline} href={url.archive}>{t('footer.downloadZIP')}</a>
@@ -69,10 +62,9 @@ const Footer = React.createClass({
           </a>
         </Row>
       </Grid>
-    );
-  }
-
-});
+    </div>
+  );
+};
 
 Footer.propTypes = {
   // mapStateToProps
