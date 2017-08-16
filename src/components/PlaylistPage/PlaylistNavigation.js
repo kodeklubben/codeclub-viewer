67abc814a209ab55cf4a850b6695a075fa58e2b5
Ole-Martin Skagevang <hero3@hotmail.com>
2017-08-16 20:50:42 +0200

    check if language is nb

--- src/components/PlaylistPage/PlaylistNavigation.js
+++ src/components/PlaylistPage/PlaylistNavigation.js
@@ -9,47 +9,51 @@ import {getTranslator} from '../../selectors/translate';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './PlaylistNavigation.scss';
 
-const PlaylistNavigation = React.createClass({
-  render() {
-    const {t} = this.props;
-    const playlists = this.props.playlists || {};
-    const playlistListItems = Object.keys(playlists).map((playlistName, idx) => {
-      const panelHeader = <h4 role="presentation">
-        <Badge pullRight>{playlists[playlistName].length}</Badge>
-        <span className={styles.link}>{playlistName}</span>
-      </h4>;
-      //const panelHeader = playlistName;
-      return playlists[playlistName].length ? <Panel key={idx} eventKey={idx} header={panelHeader}>
-        <ListGroup fill>
-          {
-            playlists[playlistName].map((lesson, idx) => {
-              return <LessonItemContainer key={idx} lesson={lesson}/>;
-            })
-          }
-        </ListGroup>
-      </Panel> : null;
-    });
+const PlaylistNavigation = ({t, playlists, language}) => {
+  const playlistsObject = playlists || {};
+  const playlistListItems = Object.keys(playlistsObject).map((name, idx) => {
+    const header = <h4 role="presentation">
+      <Badge pullRight>{playlistsObject[name].length}</Badge>
+      <span className={styles.link}>{name}</span>
+    </h4>;
+    return playlistsObject[name].length ? <Panel key={name} {...{header}} eventKey={idx}>
+      <ListGroup fill>
+        {
+          playlistsObject[name].map(lesson => {
+            return <LessonItemContainer key={lesson.path} {...{lesson}}/>;
+          })
+        }
+      </ListGroup>
+    </Panel> : null;
+  });
 
-    return playlistListItems.length ?
-      <div className={styles.container}>
-        <h3>{t('playlist.lessoncollections')}</h3>
-        <Accordion>
-          {playlistListItems}
-        </Accordion>
-      </div>
-      :
-      null;
+  if(playlistListItems.length) {
+    return language === 'nb' ?
+    <div className={styles.container}>
+      <h3>{t('playlist.lessoncollections')}</h3>
+      <Accordion>
+        {playlistListItems}
+      </Accordion>
+    </div>
+    :
+    null;
   }
-});
-
-PlaylistNavigation.propTypes = {
-  playLists: PropTypes.object,
-  t: PropTypes.func
 };
 
-function mapStateToProps(state) {
-  return {
-    t: getTranslator(state)
-  };
-}
-export default connect(mapStateToProps)(withStyles(styles)(PlaylistNavigation));
+PlaylistNavigation.propTypes = {
+  // ownProps
+  playLists: PropTypes.object,
+
+  // mapStateToProps
+  t: PropTypes.func.isRequired,
+  language: PropTypes.string.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  t: getTranslator(state),
+  language: state.language
+});
+
+export default connect(
+  mapStateToProps
+)(withStyles(styles)(PlaylistNavigation));
