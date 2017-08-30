0906731569315b27ef5829794988c36a39016d42
Ole-Martin Skagevang <hero3@hotmail.com>
2017-08-30 20:18:23 +0200

    Issue 255 and 400 moved components and fixed black blocks (#405)
    * created own files for Lesson and Readme buttons
    
    * own file for reset button
    
    * moved functions to util
    
    * Less code in Lesson.js
    
    * fix scratchblocks
    
    * deleted more and all buttons gets scss from lesson
    
    * combine lesson and readme
    
    * forgot to remove button
    
    * fixed staticfail
    
    * use selector frontmatter in mapstatetoprops
    
    * less code
    
    * make Lesson and Readme buttons own components
    
    * use InstructionButton in LessonItem.js
    
    * moved title to const
    
    * fixed warning, a inside a
    
    * CR
    
    * removed isRequired
    
    * cr. moved instruction button and added scss files

---
+++ src/components/Lesson/ReadmeButton.js
@@ -0,0 +1,28 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './ReadmeButton.scss';
+import {getTranslator} from '../../selectors/translate';
+import InstructionButton from '../InstructionButton';
+
+const ReadmeButton = ({buttonPath, t}) => {
+  const buttonText= t('lessons.toteacherinstruction');
+  const bsSize = 'small';
+  const className = styles.buttonMargin;
+  return <InstructionButton {...{className, buttonPath, buttonText, bsSize}}/>;
+};
+
+ReadmeButton.propTypes = {
+  // mapStateToProps
+  buttonPath: PropTypes.string,
+  t: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state, {path}) => ({
+  t: getTranslator(state),
+  buttonPath: (state.lessons['./' + path + '.md'] || {}).readmePath,
+});
+
+export default connect(
+  mapStateToProps
+)(withStyles(styles)(ReadmeButton));
