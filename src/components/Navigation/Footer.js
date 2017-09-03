37c165f87c3c07c58ab4e9de39df9b4d185d9059
Ole-Martin Skagevang <hero3@hotmail.com>
2017-09-03 13:55:39 +0200

    always show github and changed som styling

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -44,20 +44,17 @@ const Footer = ({t, isStudentMode}) => {
 
   return (
       <Grid fluid={true} className={isStudentMode ? styles.containerStudent : styles.containerTeacher}>
-      {isStudentMode ? null :
-        <div>
-          <Row className={styles.github}>
-            <p><a href={url.oppgaver}>{t('footer.contribute')}</a></p>
-          </Row>
-          <Row className={styles.github}>
-            <a href={url.oppgaver}>
-              <img className={styles.svg} src={require('../../assets/graphics/github-teacher.svg')}/>
-            </a>
-          </Row>
-          <Row>
-            <div className={styles.divider}/>
-          </Row>
-        </div>}
+        <Row className={styles.github}>
+          <p><a href={url.oppgaver} target="_blank">{t('footer.contribute')}</a></p>
+        </Row>
+        <Row className={styles.github}>
+          <a href={url.oppgaver} target="_blank">
+            <img className={styles.svg} src={require('../../assets/graphics/github.png')}/>
+          </a>
+        </Row>
+        <Row>
+          <div className={styles.divider}/>
+        </Row>
         {sponsors}
       </Grid>);
 };
