91e3573287b2b8b2483b579ba783a065add6b73e
Lance Olav Eastgate <lance@inspera.no>
2017-09-09 12:58:39 +0200

    Jumptosticker (#11)
    * Add zero height to jumpTo
    
    * Add valid container to AutoAffix
    
    * Refactoring
    
    * Align scroll-to to top

--- src/pages/PlaylistPage.js
+++ src/pages/PlaylistPage.js
@@ -6,6 +6,8 @@ import Col from 'react-bootstrap/lib/Col';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import AutoAffix from 'react-overlays/lib/AutoAffix';
+import cx from 'classnames';
+
 import styles from './PlaylistPage.scss';
 import {getLessonsByLevel} from '../selectors/lesson';
 import {getTranslator} from '../selectors/translate';
@@ -21,37 +23,46 @@ const PlaylistPage = ({params, lessonsByLevel, playlists, t}) => {
   const levels = Object.keys(lessonsByLevel);
   const lessonLists = levels.map(level => <LessonList key={level} {...{level}} lessons={lessonsByLevel[level]}/>);
   const filter = <LessonFilter courseName={params.course}/>;
-  const playlist = <PlaylistNavigation {...{playlists}}/>;
-  const jumpTo = <div>{levels.length > 0 ? <LevelNavigation {...{levels}}/> : null}</div>;
+  const jumpTo = levels.length > 0 ? <div><LevelNavigation {...{levels}}/></div> : null;
   const courseInfo = <CourseInfo courseName={params.course}/>;
+  const hasPlaylists = !!Object.keys(playlists).length;
+
+  let thispage = null;
+  const jumpToAffixed = jumpTo ?
+    <Col xsHidden sm={3} className={cx(styles.jumpTo, {[styles.topMargin]: lessonLists.length})}>
+      <AutoAffix viewportOffsetTop={15} container={() => thispage}>
+        {jumpTo}
+      </AutoAffix>
+    </Col> : null;
+
   return (
     <DocumentTitle title={capitalize(params.course) + ' | ' + t('title.codeclub')}>
-      <Grid fluid={true}>
+      <Grid fluid={true} ref={grid => thispage = grid}>
+
         <Row>
           <Col xs={12}><h1>{capitalize(params.course)} {t('playlist.lessons')}</h1></Col>
           <Col xs={12}>{courseInfo}</Col>
         </Row>
-        {Object.keys(playlists).length ?
+
+        {hasPlaylists ?
         <Row>
-          <Col xs={12} sm={lessonLists.length ? 9 : 12}>{playlist}</Col>
-          <Col xsHidden sm={3} className={lessonLists.length ? styles.topMargin : null}>
-            <AutoAffix viewportOffsetTop={15} container={this}>{jumpTo}</AutoAffix>
-          </Col>
+          <Col xs={12} sm={lessonLists.length ? 9 : 12}><PlaylistNavigation {...{playlists}}/></Col>
+          {jumpToAffixed}
         </Row>
         : null}
+
         <Row>
           <Col xs={12} smHidden mdHidden lgHidden>{jumpTo}</Col>
-          <Col xs={12} sm={3} className={lessonLists.length ? styles.topMargin : null}>{filter}</Col>
+          <Col xs={12} sm={3} className={cx({[styles.topMargin]: lessonLists.length})}>{filter}</Col>
           <Col xs={12} sm={6}>
-            {lessonLists.length ? lessonLists :
+            {lessonLists.length ?
+              lessonLists :
               <div className={styles.noMatchingLessons}>{t('playlist.nomatchinglessons')}</div>
             }
           </Col>
-          {Object.keys(playlists).length ? null :
-          <Col xsHidden sm={3} className={lessonLists.length ? styles.topMargin : null}>
-            <AutoAffix viewportOffsetTop={15} container={this}>{jumpTo}</AutoAffix>
-          </Col>}
+          {hasPlaylists ? null : jumpToAffixed}
         </Row>
+
       </Grid>
     </DocumentTitle>
   );
