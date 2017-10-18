2f27cd231c3d66c374caac0b7e9852bd683e764c
Ole-Martin Skagevang <hero3@hotmail.com>
2017-10-18 22:08:25 +0200

    import prop-types

--- src/components/FrontPage/Courses.js
+++ src/components/FrontPage/Courses.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
--- src/components/InstructionButton.js
+++ src/components/InstructionButton.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/lib/Button';
 import LinkContainer from 'react-router-bootstrap/lib/LinkContainer';
 
