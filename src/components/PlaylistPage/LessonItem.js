52ff22b36a0f10f3554c426f56eec404c6922482
Ole-Martin Skagevang <hero3@hotmail.com>
2017-12-27 11:25:08 +0100

    Issue 379 and 381 infobutton instead of hover and popover (#410)
    * suggestion for button instead of hover
    
    * less code
    
    * styling for anchors
    
    * more styling
    
    * styling
    
    * cr
    
    * removed instructionbutton from playlistpage
    
    * fixed instructionbutton
    
    * small fix

--- src/components/PlaylistPage/LessonItem.js
+++ src/components/PlaylistPage/LessonItem.js
@@ -10,58 +10,62 @@ import LevelIcon from '../LevelIcon';
 import {getTranslator} from '../../selectors/translate';
 import {getLessonIntro, createCheckboxesKey} from '../../util';
 import {getNumberOfCheckedCheckboxes, getTotalNumberOfCheckboxes} from '../../selectors/checkboxes';
-import TooltipComponent from '../TooltipComponent';
+import PopoverComponent from '../PopoverComponent';
 import InstructionButton from '../InstructionButton';
 
 const LessonItem = ({t, lesson, isStudentMode, checkedCheckboxes, totalCheckboxes}) => {
-  const levelIcon = <LevelIcon level={lesson.level}/>;
-
   const progressPercent = totalCheckboxes > 0 ? 100 * checkedCheckboxes / totalCheckboxes : 0;
   const progress = checkedCheckboxes > 0 ?
     <div className={styles.progress}>
       {`(${checkedCheckboxes}/${totalCheckboxes})`}
     </div> :
     null;
-
-  const instructionButton = !isStudentMode ?
-    <InstructionButton
-      className={styles.instructionBtn}
-      buttonPath={lesson.readmePath}
-      buttonText={t('playlist.instructionbutton')}
-      bsSize='xs'
-      insideLink={true}
-    />
-    : null;
-
-  const tooltipContent = getLessonIntro(lesson.path.slice(1));
-
   const progressBar = lesson.level > 0 ?
     <span className={styles['progressBarLevel' + lesson.level]} style={{width: progressPercent + '%'}}/> :
     null;
 
-  const title = <div className={styles.title}>{lesson.title}</div>;
+  const instructionButton = isStudentMode ? null :
+    <InstructionButton
+      className={styles.instructionButton}
+      buttonPath={lesson.readmePath}
+      bsSize='xs'
+      insideLink={true}
+    />;
+
+  const popoverContent = getLessonIntro(lesson.path.slice(1));
+  const popoverButton = popoverContent ?
+    <PopoverComponent {...{popoverContent}}>
+      <Glyphicon className={styles.popoverGlyph} glyph='info-sign'/>
+    </PopoverComponent>
+    : null;
 
   return (
-    <TooltipComponent {...{tooltipContent}}>
-      {lesson.external ? (
+    <div>
+      {lesson.external ?
         <ListGroupItem href={lesson.external} target="_blank" className={styles.row}>
-          {levelIcon}
-          {title}
-          &nbsp;<Glyphicon glyph="new-window"/>
-          {instructionButton}
+          <LevelIcon level={lesson.level}/>
+          <div className={styles.title}>{lesson.title}</div>
+          <Glyphicon glyph="new-window"/>
+          <span className={styles.rightSide}>
+            {instructionButton}
+            {popoverButton}
+          </span>
         </ListGroupItem>
-      ) : (
+        :
         <LinkContainer to={lesson.path}>
           <ListGroupItem className={styles.row}>
             {progressBar}
-            {levelIcon}
-            {title}
+            <LevelIcon level={lesson.level}/>
+            <div className={styles.title}>{lesson.title}</div>
             {progress}
-            {instructionButton}
+            <span className={styles.rightSide}>
+              {instructionButton}
+              {popoverButton}
+            </span>
           </ListGroupItem>
         </LinkContainer>
-      )}
-    </TooltipComponent>
+      }
+    </div>
   );
 };
 
