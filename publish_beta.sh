c4eca81f94a8e5995c8c9f36ebb980adfc41c75c
Lance Olav Eastgate <lanceolav@lanceolav.com>
2018-01-01 13:17:24 +0100

    Add use of getBetaFork, and trap errors.

--- publish_beta.sh
+++ publish_beta.sh
@@ -1,48 +1,57 @@
 #!/usr/bin/env bash
 
-cd $(dirname "$0") # Change directory to where the script is
-
-SCRIPT_PATH=$(pwd)
-CCV_PATH=${SCRIPT_PATH}
-OPPGAVER_PATH=${SCRIPT_PATH}/../oppgaver
-BETA_PATH=${SCRIPT_PATH}/../beta
-
-echo "CCV_PATH: ${CCV_PATH}"
-echo "OPPGAVER_PATH: ${OPPGAVER_PATH}"
-echo "BETA_PATH: ${BETA_PATH}"
-
-function waitForAnyKey {
-  echo #Newline
-  WAITPROMPT="Press any key to continue (or Ctrl-C to abort)..."
-  if [[ -n "$1" ]]; then
-          WAITPROMPT="$1"
-  fi
-  read -d '' -t 1 -n 10000 # Clear stdin of any keypresses, including multiple RETURNs
-  read -n1 -s -p "$WAITPROMPT" # Read 1 character silently with prompt
-  echo  #Newline
+###################################
+# Print error message and abort.
+#
+# Parameter 1 is line number (optional)
+# If parameter 1 is omitted, it will only echo "Aborting." and exit with error code 1.
+cleanupAndAbort() {
+    echo
+    if [[ $# -ge 1 ]]; then echo "Error on line $1 of $0"; fi
+    echo "Aborting."
+    cleanup
+    exit 1
 }
 
 PREPARED_PATHS=""
-function abort {
-    echo
-    echo "ABORTING!"
+function cleanup {
     for f in ${PREPARED_PATHS}; do
         restoreRepo ${f}
     done
-    exit 1
+}
+
+# Call cleanupAndAbort() if any command returns with a non-zero exit code:
+trap 'cleanupAndAbort $LINENO' ERR
+###################################
+
+
+cd $(dirname "$0") # Change directory to where the script is
+SCRIPT_PATH=$(pwd)
+
+
+function waitForAnyKey {
+    echo #Newline
+    WAITPROMPT="Press any key to continue (or Ctrl-C to abort)..."
+    if [[ -n "$1" ]]; then
+        WAITPROMPT="$1"
+    fi
+    read -d '' -t 1 -n 10000 # Clear stdin of any keypresses, including multiple RETURNs
+    read -n1 -s -p "$WAITPROMPT" # Read 1 character silently with prompt
+    echo  #Newline
 }
 
 function prepareRepo {
     echo
 
     local folder=$1
-    local giturl=$2 # e.g. kodeklubben/oppgaver.git
+    local branch=$2
+    local giturl=$3 # e.g. kodeklubben/oppgaver.git
 
     # Check that path is folder. If not: abort.
     if [[ ! -d ${folder} ]]; then
         echo "ERROR: ${folder} is not a directory."
         echo "Please clone the repo '${giturl}' to the folder ${folder}."
-        abort
+        cleanupAndAbort
     fi
 
     cd ${folder}
@@ -51,33 +60,37 @@ function prepareRepo {
     # Check that path is git folder. If not: abort.
     if [[ ! $(git rev-parse --show-toplevel 2>/dev/null) = "$(pwd)" ]]; then
         echo "ERROR: $(pwd) is not the top level folder of a git repo."
-        abort
+        cleanupAndAbort
     fi
 
     # Check that path has no uncommitted changes. If not: abort.
     if [[ -n $(git status --porcelain) ]]; then
         echo "ERROR: The repo in $(pwd) has uncommitted changes."
-        abort
+        cleanupAndAbort
     fi
 
     # Search for remote alias to $giturl. If found --> remoteAlias, otherwise abort.
     local remoteAlias=""
-    for r in $(git remote); do
-      url=$(git remote get-url ${r});
-      if [[ $(git remote get-url ${r}) == *${giturl}* ]]; then
-        remoteAlias=${r}
-        break;
-      fi
-    done
-    if [[ -z ${remoteAlias} ]]; then
-        echo "ERROR: Could not find remote alias for ${giturl}."
-        abort
+    if [[ -n ${giturl} ]]; then
+        for r in $(git remote); do
+          url=$(git remote get-url ${r});
+          if [[ $(git remote get-url ${r}) == *${giturl}* ]]; then
+            remoteAlias="${r}"
+            break;
+          fi
+        done
+        if [[ -z ${remoteAlias} ]]; then
+            echo "ERROR: Could not find remote alias for ${giturl}."
+            cleanupAndAbort
+        fi
+        echo "Found reference to ${giturl} in remote alias '${remoteAlias}'"
+        git remote update ${remoteAlias}
+        remoteAlias="${remoteAlias}/"
     fi
 
-    git remote update ${remoteAlias}
 
     # Check that ${remoteAlias}/master exists, otherwise abort.
-    local branch="${remoteAlias}/master"
+    local fullbranch="${remoteAlias}/master"
     if ! git branch --remotes | grep -q ${branch}; then
         echo "ERROR: Could not find ${branch}"
         abort
@@ -97,22 +110,43 @@ function restoreRepo {
     git checkout - # checkout previously checked out branch
 }
 
-prepareRepo ${CCV_PATH}         "kodeklubben/codeclub-viewer.git"
-prepareRepo ${OPPGAVER_PATH}    "kodeklubben/oppgaver.git"
-prepareRepo ${BETA_PATH}        "kodeklubben/beta.git"
 
-#for r in $(git remote); do
-#  url=$(git remote get-url ${r});
-#  if [[ ${url} == *"kodeklubben/codeclub-viewer.git"* ]]; then
-#    echo "Codeclub-viewer remote name: ${r}"
-#    break;
-#  fi
-#done
+#########################
+### MAIN SCRIPT START ###
+#########################
 
-#echo -n "Path to repo 'oppgaver' [default=../oppgaver]: "
-#read path_to_oppgaver
-#if [ -z "${path_to_oppgaver// }" ]; then path_to_oppgaver='../oppgaver'; fi
-#
-#echo -n "Path to repo 'beta' [default=../beta]: "
-#read path_to_beta
-#if [ -z "${path_to_beta// }" ]; then path_to_beta='../beta'; fi
+CCV_PATH=${SCRIPT_PATH}
+CCV_URL="kodeklubben/codeclub-viewer.git"
+CCV_BRANCH="master"
+
+OPPGAVER_PATH=$(dirname ${SCRIPT_PATH})/oppgaver    # ../oppgaver
+OPPGAVER_URL="kodeklubben/oppgaver.git"
+OPPGAVER_BRANCH="master"
+
+BETA_PATH=$(dirname ${SCRIPT_PATH})/beta            # ../beta
+BETA_BRANCH="gh-pages"
+
+echo -n "Attempting to get github url to beta fork (requires SSH access to github)... "
+BETA_URL=$(node getBetaFork.js)
+echo "Success!"
+echo
+echo
+echo "This script will compile kodeklubben/codeclub-viewer and kodeklubben/oppgaver and publish to ${BETA_URL}"
+echo "The script expects that the following repos have been cloned to the locations shown below."
+echo "It expects that the following repos have been cloned to the following locations:"
+echo "    (HTTPS or SSH) kodeklubben/codeclub-viewer.git    --> ${CCV_PATH}"
+echo "    (HTTPS or SSH) kodeklubben/oppgaver.git           --> ${OPPGAVER_PATH}"
+echo "    (SSH) ${BETA_URL}     --> ${BETA_PATH}"
+echo
+echo -n "Continue? [y/N]:"
+read answer
+if [ "${answer}" != "y" ]; then
+    echo "Publish aborted.";
+    exit 0
+fi
+
+prepareRepo "${CCV_PATH}" "${CCV_BRANCH}" "${CCV_URL}"
+prepareRepo "${OPPGAVER_PATH}" "${OPPGAVER_BRANCH}" "${OPPGAVER_URL}"
+prepareRepo "${BETA_PATH}" "${BETA_BRANCH}" "${BETA_URL}"
+
+cleanup
