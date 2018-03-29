dae407d59749c14752addf9076ddc92d2b28b67e
Lance Olav Eastgate <lanceolav@lanceolav.com>
2018-03-29 12:44:54 +0200

    A few suggested changes to the footer

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -45,13 +45,13 @@ const Footer = ({t, isStudentMode}) => {
 
   return (
     <Grid fluid={true} className={isStudentMode ? styles.containerStudent : styles.containerTeacher}>
-      <Row className={styles.github}>
-        <p><a href={url.oppgaver} target="_blank">{t('footer.contribute')}</a></p>
-      </Row>
-      <Row className={styles.github}>
+      <Row className={styles.githubIcon}>
         <a href={url.oppgaver} target="_blank">
           <img className={styles.svg} src={require('../../assets/graphics/github.png')}/>
         </a>
+      </Row>
+      <Row className={styles.contribute}>
+        <p><a href={url.oppgaver} target="_blank">{t('footer.contribute')}</a></p>
       </Row>
       <Row>
         <div className={styles.divider}/>
