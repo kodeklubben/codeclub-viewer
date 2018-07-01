dd32d0dc91b7d962b4a984255b5af99f836a020f
Lance Olav Eastgate <lanceolav@lanceolav.com>
2018-07-01 12:45:11 +0200

    On FrontPage, for each course show sum of lessons in all languages. If not only main language checked, show flags, and in teacher mode show number of lessons per flag.

--- src/components/CourseList/CourseItem.js
+++ src/components/CourseList/CourseItem.js
@@ -9,13 +9,16 @@ import PopoverComponent from '../PopoverComponent';
 import Flag from '../Flag';
 import LessonCount from './LessonCount';
 import {getShowFiltergroups} from '../../selectors/playlist';
-import {getCourseFrontmatter, getCourseTitle, getLanguageIndependentCoursePath} from '../../resources/courseFrontmatter';
 import {getCourseIntro} from '../../resources/courseContent';
 import {getCourseIcon} from '../../resources/courseIcon';
-import {getFilteredLessonsInCourseCountPerLanguage} from '../../selectors/lesson';
 import {onlyCheckedMainLanguage} from '../../selectors/filter';
+import {
+  getCourseFrontmatter,
+  getCourseTitle,
+  getLanguageIndependentCoursePath,
+} from '../../resources/courseFrontmatter';
 
-const CourseItem = ({course, language, showLessonCount, lessonsPerLanguage, coursePath, onlyCheckedMainLanguage}) => {
+const CourseItem = ({course, language, showLessonCount, coursePath, onlyCheckedMainLanguage}) => {
   const courseTitle = getCourseTitle(course, language);
   const {external:externalLink} = getCourseFrontmatter(course, language);
   const popoverContent = getCourseIntro(course, language);
@@ -25,19 +28,6 @@ const CourseItem = ({course, language, showLessonCount, lessonsPerLanguage, cour
       <Glyphicon className={styles.popoverGlyph} glyph='info-sign'/>
     </PopoverComponent>
     : null;
-
-  const lessonCounts = !externalLink && showLessonCount ?
-    <div>
-      {Object.keys(lessonsPerLanguage).map(language =>
-        <LessonCount
-          key={language}
-          count={lessonsPerLanguage[language]}
-          language={language}
-          showFlag={!onlyCheckedMainLanguage}
-        />)
-      }
-    </div> :
-    null;
 
   return (
     <div>
@@ -55,7 +45,7 @@ const CourseItem = ({course, language, showLessonCount, lessonsPerLanguage, cour
         <Link className={styles.courseItem} to={coursePath}>
           <img className={styles.courseLogo} src={getCourseIcon(course)} alt={courseTitle}/>
           <span className={styles.courseName}>{courseTitle}{popoverButton}</span>
-          {lessonCounts}
+          {showLessonCount ? <LessonCount {...{course}}/> : null}
         </Link>
       }
     </div>);
@@ -68,16 +58,13 @@ CourseItem.propTypes = {
 
   // mapStateToProps
   showLessonCount: PropTypes.bool.isRequired,
-  lessonsPerLanguage: PropTypes.objectOf(PropTypes.number),
   coursePath: PropTypes.string,
   onlyCheckedMainLanguage: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = (state, {course}) => {
-  const showLessonCount = getShowFiltergroups(state);
   return {
-    showLessonCount,
-    lessonsPerLanguage: showLessonCount ? getFilteredLessonsInCourseCountPerLanguage(state, course) : null,
+    showLessonCount: getShowFiltergroups(state),
     coursePath: getLanguageIndependentCoursePath(course),
     onlyCheckedMainLanguage: onlyCheckedMainLanguage(state),
   };
--- src/components/CourseList/LessonCount.js
+++ src/components/CourseList/LessonCount.js
@@ -4,29 +4,46 @@ import {connect} from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Flag from '../Flag';
 import styles from './LessonCount.scss';
-import {getAvailableLanguages} from '../../util';
 import {getTranslator} from '../../selectors/translate';
+import {getFilteredLessonsInCourseCountPerLanguage} from '../../selectors/lesson';
+import {onlyCheckedMainLanguage} from '../../selectors/filter';
 
-const LessonCount = ({count, language, showFlag, t}) => {
+const LessonCount = ({lessonsPerLanguage, showFlag, isTeacherMode, t}) => {
+  console.log(lessonsPerLanguage);
+  const totalNumberOfLessons = Object.values(lessonsPerLanguage).reduce((sum, n) => sum + n, 0);
   return (
-    <div>
-      {showFlag ? <Flag {...{language}}/> : null}
-      <span className={showFlag ? styles.flag : styles.noFlag}>{t('frontpage.lessoncount', {count})}</span>
+    <div className={styles.container}>
+      <div className={styles.center}>{t('frontpage.lessoncount', {count: totalNumberOfLessons})}</div>
+      {showFlag ?
+        <div className={styles.center}>
+          {Object.keys(lessonsPerLanguage).map(language =>
+            <span key={language} className={styles.flag}>
+              <Flag {...{language}}/>
+              <span className={styles.flagCount}>{isTeacherMode ? lessonsPerLanguage[language] : ''}</span>
+            </span>)
+          }
+        </div> :
+        null
+      }
     </div>
   );
 };
 
 LessonCount.propTypes = {
   // ownProps
-  count: PropTypes.number.isRequired,
-  language: PropTypes.oneOf(getAvailableLanguages()).isRequired,
-  showFlag: PropTypes.bool,
+  course: PropTypes.string.isRequired,
 
   // mapStateToProps
+  lessonsPerLanguage: PropTypes.objectOf(PropTypes.number),
+  showFlag: PropTypes.bool.isRequired,
+  isTeacherMode: PropTypes.bool.isRequired,
   t: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state, {course}) => ({
+  lessonsPerLanguage: getFilteredLessonsInCourseCountPerLanguage(state, course),
+  showFlag: !onlyCheckedMainLanguage(state),
+  isTeacherMode: !state.isStudentMode,
   t: getTranslator(state),
 });
 
