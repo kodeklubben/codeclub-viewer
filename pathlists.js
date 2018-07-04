c7b7869efb6b1391ffc1689e343ea04b20fbc04f
Lance Olav Eastgate <lance@inspera.no>
2018-07-04 08:46:45 +0200

    Let pathlists get lessons from data.yml

--- pathlists.js
+++ pathlists.js
@@ -9,9 +9,9 @@ const glob = require('glob');
 const lessonSrcPath = lessonSrc.replace(/\\/g, '/');
 
 /**
- *
+ * Get paths to all courses. Currently based on all "src/ * /index.md"
  * @param ending Optional ending to be added at the end of the paths
- * @returns array of strings The paths without a leading /
+ * @returns {string[]} The paths without a leading /
  */
 const coursePaths = (ending) => {
   if (typeof ending === 'undefined') { ending = ''; }
@@ -20,9 +20,10 @@ const coursePaths = (ending) => {
 };
 
 /**
+ * Get paths to all lessons. Currently based on all "src / * / * /data.yml"
  * @param {string} ending Optional ending to be added at the end of the paths
  * @param {boolean} verbose Set to true for debug info
- * @returns array of strings The paths without a leading /
+ * @returns {string[]} The paths without a leading /
  */
 const lessonPaths = (ending, verbose) => {
   if (typeof ending === 'undefined') { ending = ''; }
@@ -31,39 +32,48 @@ const lessonPaths = (ending, verbose) => {
   if (verbose) {
     console.log('Available languages:', availableLanguages);
   }
-  return glob.sync(path.join(lessonSrcPath, '*/*/*.md'))
-    .filter(p => !p.endsWith('index.md'))
-    .filter(p => {
-      try {
-        const {title, external, language} = yamlFront.loadFront(fs.readFileSync(p));
-        if (external) {
-          if (verbose) {
-            console.log('Skipping external course "' + title + '" (' + p + ')');
+
+  // Get list of all lessons as paths like 'scratch/astrokatt'
+  const lessons = glob.sync(path.join(lessonSrcPath, '*/*/data.yml')).map(p => path.dirname(p));
+
+  let paths = [];
+  for (const lesson of lessons) {
+    const lessonTranslations = glob.sync(path.join(lesson, '*.md'))
+      .filter(p => !p.endsWith('index.md'))
+      .filter(p => {
+        try {
+          const {title, external, language} = yamlFront.loadFront(fs.readFileSync(p));
+          if (external) {
+            if (verbose) {
+              console.log('Skipping external lesson "' + title + '" (' + p + ')');
+            }
+            return false;
           }
+          if (!language) {
+            if (verbose) {
+              console.warn('WARNING: Lesson "' + title + '" (' + p + ') has no language, skipping.');
+            }
+            return false;
+          }
+          if (!availableLanguages.includes(language)) {
+            if (verbose) {
+              console.log('NOTE: Lesson "' + title + '" (' + p + ') uses the language ' + language +
+                ', which is not currently available, skipping.');
+            }
+            return false;
+          }
+          //console.log('OK:', language, title, p);
+          return true;
+        }
+        catch (e) {
+          console.error('Error while processing', p, ':', e);
           return false;
         }
-        if (!language) {
-          if (verbose) {
-            console.warn('WARNING: Course "' + title + '" (' + p + ') has no language, skipping.');
-          }
-          return false;
-        }
-        if (!availableLanguages.includes(language)) {
-          if (verbose) {
-            console.log('NOTE: Course "' + title + '" (' + p + ') uses the language ' + language +
-              ', which is not currently available, skipping.');
-          }
-          return false;
-        }
-        //console.log('OK:', language, title, p);
-        return true;
-      }
-      catch (e) {
-        console.error('Error while processing', p, ':', e);
-        return false;
-      }
-    })
-    .map(p => p.replace(new RegExp(`^(${lessonSrcPath}/)(.*)(\\.md)$`), '$2' + ending));
+      })
+      .map(p => p.replace(new RegExp(`^(${lessonSrcPath}/)(.*)(\\.md)$`), '$2' + ending));
+    paths = paths.concat(lessonTranslations);
+  }
+  return paths;
 };
 
 const getStaticSitePaths = (verbose) => {
