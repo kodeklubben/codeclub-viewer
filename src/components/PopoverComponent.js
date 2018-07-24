b8f8d264fe0850668b2bc44e3845f81b3bf902b5
Skagevang <hero3@hotmail.com>
2018-07-24 16:15:15 +0200

    fixed for tooltip, removed from code tag. Only footer fix left

--- src/components/PopoverComponent.js
+++ src/components/PopoverComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import styles from './PopoverComponent.scss';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Popover from 'react-bootstrap/lib/Popover';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import {hashCode} from '../util';
 
-const PopoverComponent = ({children, popoverContent}) => {
+const PopoverComponent = ({children, popoverContent, showDyslexicFont}) => {
   const createMarkup = () => {
     return {__html: popoverContent};
   };
@@ -17,9 +18,10 @@ const PopoverComponent = ({children, popoverContent}) => {
     e.stopPropagation();
     e.preventDefault();
   };
+  const className = showDyslexicFont ? styles.dyslexia : styles.content;
   const overlay =
     <Popover id={hashCode(popoverContent)} className={styles.popover}>
-      <div className={styles.content} dangerouslySetInnerHTML={createMarkup()}/>
+      <div {...{className}} dangerouslySetInnerHTML={createMarkup()}/>
     </Popover>;
   return (
     <OverlayTrigger rootClose {...{animation, placement, trigger, onClick, overlay}}>
@@ -32,6 +34,15 @@ PopoverComponent.propTypes = {
   // ownProps
   children: PropTypes.node,
   popoverContent: PropTypes.string,
+
+  // mapStateToProps
+  showDyslexicFont: PropTypes.bool.isRequired,
 };
 
-export default withStyles(styles)(PopoverComponent);
+const mapStateToProps = (state) => ({
+  showDyslexicFont: state.showDyslexicFont,
+});
+
+export default connect(
+  mapStateToProps,
+)(withStyles(styles)(PopoverComponent));
