265bb9dae0b4f5d52585d1caa99b737e0668a488
Skagevang <hero3@hotmail.com>
2018-08-01 20:31:49 +0200

    new styling and bug fix for mode

--- src/components/Navigation/ModeDropdown.js
+++ src/components/Navigation/ModeDropdown.js
@@ -12,19 +12,20 @@ import {setModeStudent, setModeTeacher} from '../../reducers/mode';
 const ModeDropdown = ({t, setModeStudent, setModeTeacher, isStudentMode}) => {
   const modes = ['student', 'teacher'];
   const texts = {'student': t('general.student'), 'teacher': t('general.teacher')};
-  const bsStyle = isStudentMode ? modes[0] : modes[1];
-  const onSelect = () => {isStudentMode ? setModeTeacher() : setModeStudent();};
+  const mode = isStudentMode ? modes[0] : modes[1];
+  const bsStyle = 'language-' + mode;
+  const onSelect = eventKey => eventKey === 'teacher' ? setModeTeacher() : setModeStudent();
   const title = <div>
     <Glyphicon className={styles.icon} glyph={isStudentMode ? 'pencil' : 'education'}/>
     <span>
       <span className={styles.onlyMode}>{t('navbar.mode') + ': '}</span>
-      {texts[bsStyle]}
+      {texts[mode]}
     </span>
   </div>;
   return <div className={styles.gadgetContainer}>
     <DropdownButton id='mode-dropdown' noCaret pullRight {...{bsStyle, onSelect, title}}>
       {modes.map(key =>
-        <MenuItem {...{key}} eventKey={key} active={bsStyle === key}>
+        <MenuItem {...{key}} eventKey={key} active={mode === key}>
           <Glyphicon className={styles.icon} glyph={key === 'student' ? 'pencil' : 'education'}/>
           {texts[key]}
         </MenuItem>)
