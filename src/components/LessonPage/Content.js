36ef1e615e9a40362d3e4117c7157313a61e4885
Skagevang <hero3@hotmail.com>
2019-07-22 13:24:42 +0200

    moved to content

--- src/components/LessonPage/Content.js
+++ src/components/LessonPage/Content.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Content.scss';
-import {processContent} from '../../utils/processContent';
+import {processContent, renderSnippets, makeCodeInjectRenderer} from '../../utils/processContent';
 import {getLessonContent} from '../../resources/lessonContent';
 import {getLessonPath} from '../../resources/lessonFrontmatter';
 import {setCheckboxesInDoc} from '../../utils/checkboxUtils';
@@ -24,6 +24,48 @@ class Content extends React.PureComponent {
   };
 
   componentDidMount() {
+    const f = document.createElement('iframe');
+    f.id = 'makecoderenderer';
+    f.style.position = 'absolute';
+    f.style.left = 0;
+    f.style.bottom = 0;
+    f.style.width = '1px';
+    f.style.height = '1px';            
+    f.src = 'https://makecode.microbit.org/--docs?render=1&lang';
+    document.body.appendChild(f);
+    // listen for messages
+    window.addEventListener('message', ev => {
+      let msg = ev.data;
+      if (msg.source != 'makecode') return;
+      switch (msg.type) {
+        case 'renderready': {
+          // start rendering snippets!
+          let pres = document.getElementsByTagName('pre');
+          Array.prototype.forEach.call(pres, pre => {
+            const f = document.getElementById('makecoderenderer');
+            f.contentWindow.postMessage({
+              type: 'renderblocks',
+              id: pre.id,
+              code: pre.innerText
+            }, 'https://makecode.microbit.org/');
+          });
+          break;
+        }
+        case 'renderblocks': {
+          // replace text with img
+          let img = document.createElement('img');
+          img.src = msg.uri;
+          img.width = msg.width;
+          img.height = msg.height;
+          let code = document.getElementsByTagName('pre')[0];
+          if (code.className === 'microbit') {
+            code.parentElement.insertBefore(img, code);
+            code.parentElement.removeChild(code);
+          }
+          break;
+        }
+      }
+    }, false);
     if (this.props.isHydrated) { this.updateCheckboxes(); } // When clicking in from different page
   }
 
