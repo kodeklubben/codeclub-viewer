7b5c2ded348f244cc3abb08e8666c89ac3493d71
Skagevang <hero3@hotmail.com>
2019-10-24 21:26:21 +0200

    teacherinfobox done

---
+++ src/components/Filter/LessonFilter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LessonFilter = () => {
+  return (
+    <div>LessonFilter</div>
+  );
+};
+
+export default LessonFilter;
--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import {Box, Container} from '@material-ui/core';
+import {useSelector} from 'react-redux';
+import {Grid, Container} from '@material-ui/core';
+import LessonFilter from '../components/Filter/LessonFilter';
+import Courses from '../components/FrontPage/Courses';
+import TeacherInfobox from '../components/FrontPage/TeacherInfobox';
 
 const FrontPage = () => {
+  const isStudentMode = useSelector(state => state.isStudentMode);
+
   return (
     <Container>
-      <Box my={2}>
-        {[...new Array(10)]
-          .map(
-            () => `Cras mattis consectetur purus sit amet fermentum.
-  Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-  Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-  Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-          )
-          .join('\n')}
-      </Box>
+      <Grid container justify='center'>
+        {isStudentMode ? null : <TeacherInfobox/>}
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={3}>
+          <LessonFilter/>
+        </Grid>
+        <Grid item xs={12} sm={9}>
+          <Courses/>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
