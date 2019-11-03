9790b346d27f0731674bcbf9f8658de6b227e175
Lance Olav Eastgate <lanceolav@lanceolav.com>
2019-11-03 21:30:15 +0100

    Wrap lesson content in classname .lessonContent to avoid css being applied to other parts of the site by mistake.

--- src/components/LessonPage/Content.js
+++ src/components/LessonPage/Content.js
@@ -42,7 +42,7 @@ const Content = ({course, lesson, language, isReadme}) => {
   if (course === 'scratch' && typeof document !== 'undefined' && isHydrated) {
     lessonContent = renderScratchBlocks(lessonContent, styles);
   }
-  return <div dangerouslySetInnerHTML={{__html: lessonContent}}/>;
+  return <div className={styles.lessonContent} dangerouslySetInnerHTML={{__html: lessonContent}}/>;
 };
 
 Content.propTypes = {
